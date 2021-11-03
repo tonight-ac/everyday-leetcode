6d8afad7867deaedae0fa438f95ea6a152539cc2
ranzishuo <ranzishuo@bytedance.com>
2021-11-03 20:08:27 +0800

    Update

--- 1.two_sum/main.go
+++ 1.two_sum/main.go
@@ -1,7 +1,7 @@
 package main
 
 func twoSum(nums []int, target int) []int {
-	m := make(map[int]int)
+	m := make(map[int]int, len(nums))
 
 	for i, v := range nums {
 		if idx, ok := m[target-v]; ok {
