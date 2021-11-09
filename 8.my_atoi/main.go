824937b45c6f56d3438715926a52384c295d6c4d
tonight-ac <1531728675@qq.com>
2021-11-10 02:03:42 +0800

    Update

--- 8.my_atoi/main.go
+++ 8.my_atoi/main.go
@@ -1,27 +1,51 @@
 package main
 
-import (
-	"fmt"
-	"unicode"
-)
+import "fmt"
 
 func main() {
-	fmt.Println(INT32MAX)
-	fmt.Println(INT32MIN)
+	//fmt.Println(INT32MAX)
+	//fmt.Println(INT32MIN)
+	fmt.Println(myAtoi(" -42"))
 }
+
+//读入字符串并丢弃无用的前导空格
+//检查下一个字符（假设还未到字符末尾）为正还是负号，读取该字符（如果有）。 确定最终结果是负数还是正数。 如果两者都不存在，则假定结果为正。
+//读入下一个字符，直到到达下一个非数字字符或到达输入的结尾。字符串的其余部分将被忽略。
+//将前面步骤读入的这些数字转换为整数（即，"123" -> 123， "0032" -> 32）。如果没有读入数字，则整数为 0 。必要时更改符号（从步骤 2 开始）。
+//如果整数数超过 32 位有符号整数范围 [−231,  231 − 1] ，需要截断这个整数，使其保持在这个范围内。具体来说，小于 −231 的整数应该被固定为 −231 ，大于 231 − 1 的整数应该被固定为 231 − 1 。
+//返回整数作为最终结果。
 
 const INT32MAX = 2147483647
 const INT32MIN = -2147483648
 
 func myAtoi(s string) int {
-	// 处理前缀
-
-	// 处理数字
-
+	// 0 检查空格 检查符号
+	step := 0
+	char := int32(1)
 	res := int32(0)
-	char, setChar := int32(1), false
+
+	// 去除空格
+
+	// 去除符号
 	for _, v := range s {
-		if unicode.IsDigit(v) {
+		if v == ' ' && step >= 0 {
+			return 0
+		}
+
+		if (v == '-' || v == '+') && step >= 1 {
+			fmt.Println(v)
+			return 0
+		}
+
+		if v == ' ' {
+			step++
+		} else if v == '-' {
+			char = -1
+			step++
+		} else if v == '+' {
+			char = 1
+			step++
+		} else if v >= '0' && v <= '9' {
 			temp := res*10+char*(v-'0')
 			if res != temp/10 {
 				if char == 1 {
@@ -30,15 +54,6 @@ func myAtoi(s string) int {
 				return INT32MIN
 			}
 			res = temp
-		} else if unicode.IsSpace(v) {
-			continue
-		} else if string(v) == "-" {
-			if setChar { return 0 }
-			char, setChar = -1, true
-		} else if string(v) == "+" {
-			if setChar { return 0 }
-			char, setChar = 1, true
-			continue
 		} else {
 			break
 		}
