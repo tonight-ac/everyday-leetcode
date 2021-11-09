faf841ce00d9de4a6ddffd9e90eb13c5692f2c47
tonight-ac <1531728675@qq.com>
2021-11-10 01:12:53 +0800

    Updat

--- 8.my_atoi/main.go
+++ 8.my_atoi/main.go
@@ -5,32 +5,39 @@ import (
 	"unicode"
 )
 
-const INT32MAX = ^uint32(0)
-const INT32MIN = uint32(0)
-
 func main() {
-	fmt.Println(int32(INT32MAX))
-	fmt.Println(int32(INT32MIN))
+	fmt.Println(INT32MAX)
+	fmt.Println(INT32MIN)
 }
 
+const INT32MAX = 2147483647
+const INT32MIN = -2147483648
+
 func myAtoi(s string) int {
+	// 处理前缀
+
+	// 处理数字
+
 	res := int32(0)
-	char := int32(1)
+	char, setChar := int32(1), false
 	for _, v := range s {
 		if unicode.IsDigit(v) {
-			if temp := res * 10 + char*(v-'0'); res != temp/10 {
+			temp := res*10+char*(v-'0')
+			if res != temp/10 {
 				if char == 1 {
-					return int(INT32MAX)
+					return INT32MAX
 				}
-				return int(INT32MIN)
+				return INT32MIN
 			}
-			res *= 10
-			res += char*(v-'0')
+			res = temp
 		} else if unicode.IsSpace(v) {
 			continue
 		} else if string(v) == "-" {
-			char = -1
+			if setChar { return 0 }
+			char, setChar = -1, true
 		} else if string(v) == "+" {
+			if setChar { return 0 }
+			char, setChar = 1, true
 			continue
 		} else {
 			break
