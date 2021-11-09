03ee1f2bab97e052c69b173a0615d639df438f7c
tonight-ac <1531728675@qq.com>
2021-11-10 00:08:36 +0800

    Update

--- 8.my_atoi/main.go
+++ 8.my_atoi/main.go
@@ -1,9 +1,33 @@
 package main
 
-func myAtoi(s string) int {
-	//for _, v := range s {
-	//
-	//}
+import "unicode"
 
-	return 0
+const UINT32_MIN uint32 = 0
+const UINT32_MAX uint32 = ^uint32(0)
+
+func myAtoi(s string) int {
+	res := int32(0)
+	char := int32(1)
+	for _, v := range s {
+		if unicode.IsDigit(v) {
+			if temp := res * 10 + char*(v-'0'); res != temp/10 {
+				if char == 1 {
+					return int(UINT32_MAX)
+				}
+				return int(UINT32_MIN)
+			}
+			res *= 10
+			res += char*(v-'0')
+		} else if unicode.IsSpace(v) {
+			continue
+		} else if string(v) == "-" {
+			char = -1
+		} else if string(v) == "+" {
+			continue
+		} else {
+			break
+		}
+	}
+
+	return int(res)
 }
