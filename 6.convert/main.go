cdc0a1a7f380afc64d52047f4afc92cdd6545399
ranzishuo <ranzishuo@bytedance.com>
2021-11-09 14:25:27 +0800

    Updat

--- 6.convert/main.go
+++ 6.convert/main.go
@@ -1,7 +1,24 @@
 package main
 
 func convert(s string, numRows int) string {
+	// 1       9 2 * numRows - 2
+	// 2     8 1
+	// 3   7
+	// 4 6
+	// 5
+	cycle := 2 * numRows - 2
+	res := ""
 
+	for row := 0; row < numRows; row++ {
+		diff := cycle - 2 * numRows
+		for idx := row; idx < len(s); {
+			res += string(s[idx])
+			idx += diff
+			if diff != cycle {
+				diff = cycle - diff
+			}
+		}
+	}
 
-	return ""
+	return res
 }
