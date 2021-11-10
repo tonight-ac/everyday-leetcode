f4936c7d2e1e6a99d0f438e7093845393d81b931
ranzishuo <ranzishuo@bytedance.com>
2021-11-10 15:16:18 +0800

    Update

--- 14.longest_common_prefix/main.go
+++ 14.longest_common_prefix/main.go
@@ -1,28 +1,44 @@
 package main
 
+import "fmt"
+
+func main() {
+	fmt.Println(longestCommonPrefix([]string{"a","flow","flight"}))
+}
+
 //输入：strs = ["flower","flow","flight"]
 //输出："fl"
 
 func longestCommonPrefix(strs []string) string {
-	// 既然len(strs) > 0, 怎选取第一个作为模版
+	// 选取第一个作为模版，不要判空，入参必有一个
 	p := strs[0]
 
 	// 对p的每个字符计数用的list
-	counts := make([]int, len(p))
+	// int8节约内存，题目规定入参数组长度不超过200
+	counts := make([]int8, len(p))
 
 	// 计算最大长度
-	max := 0
+	min := len(p)
 
 	for i := 0; i < len(strs); i++ {
 		s := strs[i]
-		// 存在空串就不需要比较了
+
+		// 存在空串就不需要比较了，因为没有前缀
 		if len(s) == 0 { return "" }
-		for j := 0; j < len(s) && j < len(p); j++ {
-			if s[j] == p[j] {
-				counts[j]++
+
+		j := 0
+		for ; j < len(s) && j < len(p); j++ {
+			if s[j] != p[j] {
+				break
 			}
+			counts[j]++
+		}
+
+		// 循环结束时计算最大前缀
+		if j < min {
+			min = j
 		}
 	}
 
-	return p[0:max]
+	return p[0:min]
 }
