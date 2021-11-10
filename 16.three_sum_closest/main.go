da17a69eddbe30395d2d981ee604421a99521285
ranzishuo <ranzishuo@bytedance.com>
2021-11-10 20:00:08 +0800

    Update

--- 16.three_sum_closest/main.go
+++ 16.three_sum_closest/main.go
@@ -1,9 +1,15 @@
 package main
 
+import "fmt"
+
 //输入：nums = [-1,2,1,-4], target = 1
 //输出：2
 //解释：与 target 最接近的和是 2 (-1 + 2 + 1 = 2) 。
 // -4 -1 1 2
+
+func main() {
+	fmt.Println(threeSumClosest([]int{-1,2,1,-4}, 1))
+}
 
 func threeSumClosest(nums []int, target int) int {
 	// 把前三个切下来 尝试一下贪心算法
