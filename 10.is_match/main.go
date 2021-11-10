0b5d610f81ba40b9f65f017e44203f79ce3c7cd3
ranzishuo <ranzishuo@bytedance.com>
2021-11-10 11:09:59 +0800

    Update

---
+++ 10.is_match/main.go
@@ -0,0 +1,13 @@
+package main
+
+//输入：s = "mississippi" p = "mis*is*p*."
+//输出：false
+
+func isMatch(s string, p string) bool {
+	if len(s) == 0 || len(p) == 0 {
+		return false
+	}
+
+
+	return true
+}
