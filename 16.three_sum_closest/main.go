894384ce0e0aebca09e3261056a1a4dd346b47dd
ranzishuo <ranzishuo@bytedance.com>
2021-11-10 20:21:25 +0800

    Update

--- 16.three_sum_closest/main.go
+++ 16.three_sum_closest/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 //输入：nums = [-1,2,1,-4], target = 1
 //输出：2
@@ -8,29 +11,46 @@ import "fmt"
 // -4 -1 1 2
 
 func main() {
-	fmt.Println(threeSumClosest([]int{-1,2,1,-4}, 1))
+	fmt.Println(threeSumClosest([]int{-55,-24,-18,-11,-7,-3,4,5,6,9,11,23,33}, 0))
 }
+// -10
 
+//[-55,-24,-18,-11,-7,-3,4,5,6,9,11,23,33]
+//0
+//输出：
+//2
+//预期结果：
+//0
+
+//[1,2,4,8,16,32,64,128]
+//82
+//输出：
+//88
+//预期结果：
+//82
+
+// 这里使用贪心，无法解决的问题在于，最接近的是0，存在刚好相等的情况
 func threeSumClosest(nums []int, target int) int {
+	sort.Ints(nums)
+
 	// 把前三个切下来 尝试一下贪心算法
-	tuple := append(make([]int, 0), nums[0:3]...)
+	tuple := append(make([]int, 0), nums[len(nums)-3:]...)
 	res := tuple[0] + tuple[1] + tuple[2]
 	diff := abs(res - target)
 
 	// 从第四个开始遍历
-	for i := 3; i < len(nums); i++ {
+	for i := len(nums)-4; i >= 0; i-- {
 		idx := -1
 		min := -1
 		for j := 0; j < len(tuple); j++ {
 			if temp := abs(res - tuple[j] + nums[i] - target); temp < diff {
 				// 初始化min 或者取更小的min
-				if min < 0 || min > diff - temp {
+				if min < 0 || min < diff - temp {
 					min = diff - temp
 					idx = j
 				}
 			}
 		}
-
 		if idx != -1 {
 			// swap
 			tuple[idx] = nums[i]
