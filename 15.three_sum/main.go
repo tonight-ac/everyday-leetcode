b5e71b8448525f366c36cf15b7531fde0171014f
ranzishuo <ranzishuo@bytedance.com>
2021-11-10 16:02:01 +0800

    Update

--- 15.three_sum/main.go
+++ 15.three_sum/main.go
@@ -1,21 +1,33 @@
 package main
 
-import "sort"
+import (
+	"fmt"
+	"sort"
+)
 
+func main() {
+	fmt.Println(threeSum([]int{-2,0,1,1,2}))
+}
 //0 <= nums.length <= 3000
 //-105 <= nums[i] <= 105
 
 func threeSum(nums []int) [][]int {
 	res := make([][]int, 0)
 
+	if len(nums) < 3 {
+		return res
+	}
+
 	//输入：nums = [-1,0,1,2,-1,-4]
 	//输出：[[-1,-1,2],[-1,0,1]]
+
+	// -4 -1 -1 0 1 2
 
 	// 先做排序，不排序的话，无法进行，且无法去重
 	sort.Ints(nums)
 
 	// 首尾两个指针，同时i < j+1需要留出三个数字的空间
-	for i, j := 0, len(nums)-1; i < j+1; i, j = i+1, j-1 {
+	for i, j := 0, len(nums)-1; i + 1 < j; {
 		// 求二数之和取反，然后在i+1, j-1的范围内二分找sum
 		sum := -(nums[i] + nums[j])
 
@@ -34,12 +46,19 @@ func threeSum(nums []int) [][]int {
 		}
 
 		// 去除重复元素
-		for i < j + 1 && nums[i] == nums[i+1] {
+		for i + 1 < j && nums[i] == nums[i+1] {
 			i++
 		}
 		// 去除重复元素
-		for i < j + 1 && nums[j] == nums[j-1] {
+		for i + 1 < j && nums[j] == nums[j-1] {
 			j--
+		}
+
+		// 两数之和为正，说明nums[j]较大，需要像小的方向移动
+		if nums[i] + nums[j] > 0 {
+			j--
+		} else { // 同理为负，说明较小，需要像大的方向移动
+			i++
 		}
 	}
 
