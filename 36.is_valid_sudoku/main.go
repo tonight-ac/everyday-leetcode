e92c9f31625797af024cab6f5a8acebf6b606a9c
ranzishuo <ranzishuo@bytedance.com>
2021-11-11 20:07:09 +0800

    Update

---
+++ 36.is_valid_sudoku/main.go
@@ -0,0 +1,16 @@
+package main
+
+func isValidSudoku(board [][]byte) bool {
+	row := make([]map[byte]interface{}, 9)
+	clo := make([]map[byte]interface{}, 9)
+	cube := make([]map[byte]interface{}, 9)
+	//
+
+	for i, r := range board {
+		for j, v := range r {
+			// 对v同时更新三个区间
+			//row[i][v] = interface{}{}
+		}
+	}
+}
+
