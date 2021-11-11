f5a2c2c12780509686d47cb663b84532696e56c9
ranzishuo <ranzishuo@bytedance.com>
2021-11-11 16:09:22 +0800

    Update

--- 17.letter_combinations/main.go
+++ 17.letter_combinations/main.go
@@ -1,8 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-var m = map[int32]string{
+//"2"
+//输出：
+//["ad","ae","af","bd","be","bf","cd","ce","cf","a","b","c"]
+//预期结果：
+//["a","b","c"]
+
+func main() {
+	fmt.Println(letterCombinations("2"))
+}
+
+var m = map[uint8]string{
 	'2': "abc",
 	'3': "def",
 	'4': "ghi",
@@ -12,26 +24,56 @@ var m = map[int32]string{
 	'8': "tuv",
 	'9': "wxyz",
 }
-
-var res = make([]string, 0)
+//var s string
+//func letterCombinations(digits string) []string {
+//	// 递归结束条件
+//	if digits == "" {
+//		if s != "" {
+//			return []string{s}
+//		}
+//	}
+//
+//	res := make([]string, 0)
+//
+//	for _, v := range m[digits[0]] {
+//		// 插入元素
+//		s += string(v)
+//		// 递归
+//		res = append(res, letterCombinations(digits[1:])...)
+//		// 删除元素
+//		s = s[:len(s)-1]
+//	}
+//
+//	return res
+//}
+var res []string
 
 // 尝试做一个递归
 func letterCombinations(digits string) []string {
-	// 递归结束条件
-	if digits == "" {
-		return nil
-	}
+	if digits == "" { return nil }
+	// 4 4 4 4
+	//
+	res = make([]string, 0)
 
-	com := m[int32(digits[0])]
-
-	for _, v := range com {
-		// 插入元素
-		fmt.Println(v)
-		// 递归
-		_ = letterCombinations(digits[1:])
-		// 删除元素
-
-	}
+	recursion(digits, "")
 
 	return res
+}
+
+
+func recursion(digits string, s string) {
+	// 递归结束条件
+	if digits == "" {
+		res = append(res, s)
+		return
+	}
+
+	for _, v := range m[digits[0]] {
+		// 插入元素
+		s += string(v)
+		// 递归
+		recursion(digits[1:], s)
+		// 删除元素
+		s = s[:len(s)-1]
+	}
 }
