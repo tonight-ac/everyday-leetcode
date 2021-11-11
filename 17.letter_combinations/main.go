ddf2addc8003ed4c76eb7890b5909ebf59037cca
ranzishuo <ranzishuo@bytedance.com>
2021-11-11 17:02:56 +0800

    Update

--- 17.letter_combinations/main.go
+++ 17.letter_combinations/main.go
@@ -13,64 +13,48 @@ import (
 func main() {
 	fmt.Println(letterCombinations("2"))
 }
-// TODO 未发题解
-var m = map[uint8]string{
-	'2': "abc",
-	'3': "def",
-	'4': "ghi",
-	'5': "jkl",
-	'6': "mno",
-	'7': "pqrs",
-	'8': "tuv",
-	'9': "wxyz",
+
+// 使用字符串数组，提前创建好映射关系
+var m = map[uint8][]string{
+	'2': {"a", "b", "c"},
+	'3': {"d", "e", "f"},
+	'4': {"g", "h", "i"},
+	'5': {"j", "k", "l"},
+	'6': {"m", "n", "o"},
+	'7': {"p", "q", "r", "s"},
+	'8': {"t", "u", "v"},
+	'9': {"w", "x", "y", "z"},
 }
-//var s string
-//func letterCombinations(digits string) []string {
-//	// 递归结束条件
-//	if digits == "" {
-//		if s != "" {
-//			return []string{s}
-//		}
-//	}
-//
-//	res := make([]string, 0)
-//
-//	for _, v := range m[digits[0]] {
-//		// 插入元素
-//		s += string(v)
-//		// 递归
-//		res = append(res, letterCombinations(digits[1:])...)
-//		// 删除元素
-//		s = s[:len(s)-1]
-//	}
-//
-//	return res
-//}
-var res []string
+
+// 最多4位，组合出256种情况
+var res = make([]string, 256)
+// 为了节约内存，开了固定长度res，使用end计算每轮结果最后的下标位置
+var end int
 
 // 尝试做一个递归
 func letterCombinations(digits string) []string {
 	if digits == "" { return nil }
-	// 4 4 4 4
-	//
-	res = make([]string, 0)
+
+	// 初始化下标，不然全局变量在case之间会互相污染
+	end = 0
 
 	recursion(digits, "")
 
-	return res
+	return res[:end]
 }
 
 
 func recursion(digits string, s string) {
 	// 递归结束条件
 	if digits == "" {
-		res = append(res, s)
+		res[end] = s
+		end++
 		return
 	}
 
 	for _, v := range m[digits[0]] {
 		// 插入元素
-		s += string(v)
+		s += v
 		// 递归
 		recursion(digits[1:], s)
 		// 删除元素
