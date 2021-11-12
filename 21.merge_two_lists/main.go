a9c54976ece5319e8809164f4f38b046eee8c926
tonight-ac <1531728675@qq.com>
2021-11-13 03:16:10 +0800

    Update

--- 21.merge_two_lists/main.go
+++ 21.merge_two_lists/main.go
@@ -6,8 +6,31 @@ type ListNode struct {
 }
 
 func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
-	//head := &ListNode{}
+	// 均为空，则结果必为空
+	if l1 == nil && l2 == nil { return nil }
 
+	// 加一个头节点，处理起来更方便
+	head := &ListNode{}
+	node := head
 
-	return nil
+	// 两个链表长度可能不登长，先处理公共长部分
+	for l1 != nil && l2 != nil {
+		if l1.Val <= l2.Val {
+			node.Next = l1 // 添加l1
+			l1 = l1.Next
+		} else {
+			node.Next = l2 // 添加l2
+			l2 = l2.Next
+		}
+		node = node.Next
+	}
+
+	// 最后处理可能超出的部分
+	if l1 != nil {
+		node.Next = l1
+	} else {
+		node.Next = l2
+	}
+
+	return head.Next
 }
