fa3f78b99966a869996c9e18f9f7de7da00c4f51
ranzishuo <ranzishuo@bytedance.com>
2021-11-12 15:58:24 +0800

    Update

--- 22.generate_arenthesis/main.go
+++ 22.generate_arenthesis/main.go
@@ -1,24 +1,38 @@
 package main
 
-import (
-	"strings"
-)
+import "fmt"
 
-// 2 <= n <= 8
-func generateParenthesis(n int) []string {
-
-	return nil
+func main() {
+	fmt.Println(generateParenthesis(3))
 }
 
-func recursion(n, left, right int, b *strings.Builder, s string) {
-	b.WriteString(s)
+// 2 <= n <= 8
+//输入：n = 3
+//输出：["((()))","(()())","(())()","()(())","()()()"]
 
-	if left + right == n {
+var tot int
+var res []string
+func generateParenthesis(n int) []string {
+	res = make([]string, 0)
+	tot = n
+	recursion(0, 0, "")
+	return res
+}
 
+func recursion(left, right int, s string) {
+	if len(s) == 2 * tot {
+		res = append(res, s)
 		return
 	}
 
-	if left < n/2 {
-		recursion(n, left + 1, right, b, "(")
+	if left < tot {
+		s += "("
+		recursion(left+1, right, s)
+		s = s[:len(s)-1]
+	}
+	if left <= tot && right < left {
+		s += ")"
+		recursion(left, right+1, s)
+		s = s[:len(s)-1]
 	}
 }
