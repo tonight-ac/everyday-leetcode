3ef8f0ccb394376e2ecff0ada916f112be2cf631
tonight-ac <1531728675@qq.com>
2021-11-14 21:43:03 +0800

    Update

--- 25.reverse_k_group/main.go
+++ 25.reverse_k_group/main.go
@@ -7,6 +7,10 @@ type ListNode struct {
 
 //输入：head = [1,2,3,4,5], k = 2
 //输出：[2,1,4,3,5]
+
+// 1 2 3 4 5
+// 截取1 2
+// 反转 再拼接回去
 func reverseKGroup(head *ListNode, k int) *ListNode {
 	// 常见手段，加一个头节点
 	head = &ListNode{ Next: head }
@@ -15,23 +19,26 @@ func reverseKGroup(head *ListNode, k int) *ListNode {
 	for start, end, cnt := head, head, 0; end != nil; end, cnt = end.Next, cnt + 1 {
 		if cnt == k {
 			// 截断返回
-			//
-			temp := end.Next
+			// 记录下3
+			nextFirst := end.Next
 			end.Next = nil
-			_ = reverseList(start.Next)
+			// 归位end
 			end = start.Next
-			end.Next = temp
 
-			// start.Next目前是尾巴了
-			start.Next.Next = temp
+			// 反转1 -> 2
+			thisFirst := reverseList(start.Next)
 
-			// end目前头部了
-			start.Next = end
+			// 把反转后的链表拼接回去
+			// start.Next已经反转到末尾，接上 1 -> 3
+			start.Next.Next = nextFirst
+			// 连接上0 —> 2
+			start.Next = thisFirst
+			// 把start也归位
+			start = end
 
 			// 更新cnt进行下一个计数周期
 			cnt = 0
 		}
-
 	}
 
 	return head.Next
@@ -50,5 +57,3 @@ func reverseList(head *ListNode) *ListNode {
 
 	return p
 }
-
-
