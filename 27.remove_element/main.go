be10db502d0e840ac58b109f3b27c0ce555b7427
tonight-ac <1531728675@qq.com>
2021-11-14 22:15:32 +0800

    Update

--- 27.remove_element/main.go
+++ 27.remove_element/main.go
@@ -2,8 +2,10 @@ package main
 
 import "fmt"
 
+// 0,1,2,2,2,2,2,3,4
+// 0 1 4 3
 func main() {
-	list := []int{0,1,2,2,3,0,4,2}
+	list := []int{0,1,2,2,2,2,2,3,4}
 	fmt.Println(removeElement(list, 2))
 	fmt.Println(list)
 }
@@ -12,23 +14,32 @@ func removeElement(nums []int, val int) int {
 	if len(nums) == 0 { return 0 }
 
 	// i为待替换的，j为待填充的
-	var i, j int
-	for i, j = 0, len(nums)-1; i < j; {
-		fmt.Println(i, j)
+	for i, j := 0, len(nums)-1; i < j; {
+		// 可以换位置，更换
+		if nums[i] == val && nums[j] != val {
+			nums[i] = nums[j]
+			nums[j] = val
+			i++
+			j--
+		}
+		// 不符合移动i向后寻找待更换位置
 		if nums[i] != val {
 			i++
 		}
 		if nums[j] == val {
 			j--
 		}
-		if nums[i] == val && nums[j] != val {
-			nums[i] = nums[j]
-			i++
-			j--
+	}
+
+	// 通过i、j去计算结果，不方便，干脆单独计算
+	res := 0
+	for _, v := range nums {
+		if v != val {
+			res++
+		} else {
+			break
 		}
 	}
 
-	//if nums[i] == val { return 0 }
-
-	return i+1
+	return res
 }
