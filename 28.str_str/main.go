a3b175068f9e0e4c39a783f94a3108cc11b5e7aa
ranzishuo <ranzishuo@bytedance.com>
2021-11-15 12:20:53 +0800

    Update

--- 28.str_str/main.go
+++ 28.str_str/main.go
@@ -4,8 +4,33 @@ package main
 //输出：2
 // 简单来说就是最经典的kmp算法，课本上都会讲
 func strStr(haystack string, needle string) int {
+	n, m := len(haystack), len(needle)
 
-	return 0
+	if m == 0 { return 0 }
+
+	//输入：haystack = "hello", needle = "ll"
+	//输出：2
+
+	pi := make([]int, m)
+	for i, j := 1, 0; i < m; i++ {
+		for j > 0 && needle[i] != needle[j] {
+			j = pi[j - 1]
+		}
+		if needle[i] == needle[j] {
+			j++
+		}
+		pi[i] = j
+	}
+
+	for i, j := 0, 0; i < n; i++ {
+		for j > 0 && haystack[i] != needle[j] {
+			j = pi[j - 1]
+		}
+		if haystack[i] == needle[j] { j++ }
+		if j == m { return i - m + 1 }
+	}
+
+	return -1
 }
 
 //func strStr(haystack string, needle string) int {
