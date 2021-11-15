e68236148ff3d189d997d1ccde5c2618df2fde58
ranzishuo <ranzishuo@bytedance.com>
2021-11-15 12:44:41 +0800

    Update

--- 28.str_str/main.go
+++ 28.str_str/main.go
@@ -1,5 +1,13 @@
 package main
 
+import "fmt"
+
+func main() {
+	strStr("", "ABCABCABCD")
+	// ABCABCD
+	// [0 0 0 1 2 3 0]
+}
+
 //输入：haystack = "hello", needle = "ll"
 //输出：2
 // 简单来说就是最经典的kmp算法，课本上都会讲
@@ -11,20 +19,25 @@ func strStr(haystack string, needle string) int {
 	//输入：haystack = "hello", needle = "ll"
 	//输出：2
 
-	pi := make([]int, m)
+	// 计算next数组
+	// next表示的含义是，如果在needle在这个位置失去和haystack的匹配了，如果保持haystack的指针不变，移动needle指针的位置
+	// ABCABCD
+	next := make([]int, m)
 	for i, j := 1, 0; i < m; i++ {
+		fmt.Println(i, j)
 		for j > 0 && needle[i] != needle[j] {
-			j = pi[j - 1]
+			j = next[j - 1]
 		}
 		if needle[i] == needle[j] {
 			j++
 		}
-		pi[i] = j
+		next[i] = j
 	}
+	fmt.Println(next)
 
 	for i, j := 0, 0; i < n; i++ {
 		for j > 0 && haystack[i] != needle[j] {
-			j = pi[j - 1]
+			j = next[j - 1] // j失败，选取j-1的位置
 		}
 		if haystack[i] == needle[j] { j++ }
 		if j == m { return i - m + 1 }
