b5a7f09a8d76a2a21f36d4c2c8aae68ffd0590de
ranzishuo <ranzishuo@bytedance.com>
2021-11-15 18:32:00 +0800

    Update

--- 33.search/main.go
+++ 33.search/main.go
@@ -10,21 +10,17 @@ func main() {
 
 func search(nums []int, target int) int {
 	// 先通过二分法判断峰值位置
+	mid := 0
 	for l, r := 0, len(nums)-1; l <= r; {
-		mid := (l + r) / 2
-		fmt.Println(l, r)
-		fmt.Println(mid)
-		if target < nums[mid] {
-			if nums[l] < target {
-
-			}
-			r = mid - 1
-		} else if target > nums[mid] {
+		mid = (l + r) / 2
+		if nums[mid] > nums[mid+1] {
+			break
+		} else if nums[mid] < nums[mid+1] {
 			l = mid + 1
-		} else {
-			return mid
 		}
 	}
+
+	fmt.Println(mid)
 
 	// 然后再左侧或右侧再二分搜索
 	for l, r := 0, len(nums)-1; l <= r; {
