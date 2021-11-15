fb2b4acaa6e84645de241b383a500c92c8777816
tonight-ac <1531728675@qq.com>
2021-11-15 22:28:25 +0800

    Update

--- 46.permute/main.go
+++ 46.permute/main.go
@@ -3,13 +3,31 @@ package main
 //输入：nums = [1,2,3]
 //输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
 
-
+var list []int
+var res [][]int
 func permute(nums []int) [][]int {
+	list = nums
 
+	res = make([][]int, 0)
 
-	return nil
+	recursion([]int{}, make(map[int]bool))
+
+	return res
 }
 
-func recursion() {
+func recursion(r []int, m map[int]bool) {
+	if len(r) == len(list) {
+		res = append(res, append(make([]int, 0), r...))
+		return
+	}
 
+	for i := 0; i < len(list); i++ {
+		if !m[list[i]] {
+			r = append(r, list[i])
+			m[list[i]] = true
+			recursion(r, m)
+			r = r[:len(r)-1]
+			m[list[i]] = false
+		}
+	}
 }
