a2ae297f322ccaa6a2a813ce052add929ad21697
ranzishuo <ranzishuo@bytedance.com>
2021-11-15 17:54:28 +0800

    Update

--- 30.find_substring/main.go
+++ 30.find_substring/main.go
@@ -2,43 +2,78 @@ package main
 
 import "fmt"
 
-func main() {
-	fmt.Println(findSubstring("barfoothefoobarman", []string{"bar", "foo"}))
-}
-
 //"barfoofoobarthefoobarman"
 //["bar","foo","the"]
 //输出：
 //[9]
 //预期结果：
 //[6,9,12]
-// TODO 试一下递归吧
 
+// 应该存在类似kmp算法的类似解法
+func main() {
+	fmt.Println(findSubstring("barfoothefoobarman", []string{"word","good","best","good"}))
+}
+//"wordgoodgoodgoodbestword"
+//["word","good","best","good"]
+//输出：
+//[]
+//预期结果：
+//[8]
+// 递归解法
+var m map[string]int
+var length int
 func findSubstring(s string, words []string) []int {
 	// 几步走？
-	// 1 当前下标处
+	// 1 当前是否匹配
 
-	return nil
-}
-
-func recursion(m map[string]bool, s string, count int) {
-	if count == 3 {
-		return
+	// words是可能重复的
+	m = make(map[string]int, len(words))
+	for _, v := range words {
+		m[v] = m[v] + 1
 	}
 
-	// 当前匹配
-	if v, ok := m[s[0:3]]; ok && !v {
-		// match 上之后继续向下
-		m[s[0:3]] = true
-		recursion(m, s[3:], count+1)
-		m[s[0:3]] = false
+	length = len(words[0])
+
+	size := len(words)*length
+
+	res := make([]int, 0)
+
+	for i := 0; i <= len(s)-size; i++ {
+		// 处理i~i+size的字符
+		// 长度不然是这么长，我们不判断什么符合，我们只判断什么是异常情况
+		if recursion(s[i:i+size]) {
+			res = append(res, i)
+		}
 	}
 
-	// 不匹配继续向下
-	recursion(m, s[1:], count)
-
+	return res
 }
 
+func recursion(s string) bool {
+	if s == "" { return true }
+
+	ss := s[:length]
+	if m[ss] - 1 < 0 {
+		return false
+	}
+
+	m[ss]--
+	res := recursion(s[length:])
+	m[ss]++
+
+	return res
+}
+
+//"barfoofoobarthefoobarman"
+//["bar","foo","the"]
+//输出：
+//[9,12]
+//预期结果：
+//[6,9,12]
+//func main() {
+//	fmt.Println(findSubstring("barfoofoobarthefoobarman", []string{"bar", "foo", "the"}))
+//}
+//
 //func findSubstring(s string, words []string) []int {
 //	// 把words组装成一个map，方便搜索
 //	m := make(map[string]bool, len(words))
@@ -54,6 +89,12 @@ func recursion(m map[string]bool, s string, count int) {
 //	// 循环里新开一个局部去重map
 //	uni := make(map[string]bool, len(words))
 //	for i := 0; i <= len(s)-length; {
+//		//fmt.Println(i)
+//		//fmt.Println(s[i:i+length])
+//		if uni[s[i:i+length]] {
+//			count = 0
+//			uni = make(map[string]bool, len(words))
+//		}
 //		if m[s[i:i+length]] && !uni[s[i:i+length]] {
 //			uni[s[i:i+length]] = true
 //			count++
