8e51694eba818b3f395928b5a2a8a52fb0a82131
ranzishuo <ranzishuo@bytedance.com>
2021-11-15 18:41:59 +0800

    Update

--- 33.search/main.go
+++ 33.search/main.go
@@ -8,10 +8,19 @@ func main() {
 	fmt.Println(search([]int{4,5,6,7,0,1,2}, 0))
 }
 
+//输入：
+//[4,5,6,7,0,1,2]
+//3
+//输出：
+//0
+//预期结果：
+//-1
 func search(nums []int, target int) int {
+	if len(nums) == 0 { return -1 }
+
 	// 先通过二分法判断峰值位置
-	mid := 0
-	for l, r := 0, len(nums)-1; l <= r; {
+	l, r, mid := 0, 0, 0
+	for l, r = 0, len(nums)-1; l <= r; {
 		mid = (l + r) / 2
 		if nums[mid] > nums[mid+1] {
 			break
@@ -20,11 +29,17 @@ func search(nums []int, target int) int {
 		}
 	}
 
-	fmt.Println(mid)
+	if target < nums[len(nums)-1] {
+		l, r = mid + 1, len(nums)-1
+	} else if target > nums[len(nums)-1] {
+		l, r = 0, mid - 1
+	} else {
+		return mid
+	}
 
 	// 然后再左侧或右侧再二分搜索
-	for l, r := 0, len(nums)-1; l <= r; {
-		mid := (l + r) / 2
+	for l <= r {
+		mid = (l + r) / 2
 		if target < nums[mid] {
 			r = mid - 1
 		} else if target > nums[mid] {
