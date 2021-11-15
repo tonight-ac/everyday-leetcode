4ac680c59ec4cb8809373303716cff007a2ac37d
ranzishuo <ranzishuo@bytedance.com>
2021-11-15 13:03:33 +0800

    Updae

--- 28.str_str/main.go
+++ 28.str_str/main.go
@@ -23,6 +23,7 @@ func strStr(haystack string, needle string) int {
 	// next表示的含义是，如果在needle在这个位置失去和haystack的匹配了，如果保持haystack的指针不变，移动needle指针的位置
 	// ABCABCD
 	next := make([]int, m)
+	// 自己先和自己匹配一遍，求出next数组
 	for i, j := 1, 0; i < m; i++ {
 		fmt.Println(i, j)
 		for j > 0 && needle[i] != needle[j] {
@@ -36,10 +37,13 @@ func strStr(haystack string, needle string) int {
 	fmt.Println(next)
 
 	for i, j := 0, 0; i < n; i++ {
+		// 不是归0的话，就一直循环回退，直到找到合适的haystack[i] != needle[j]
 		for j > 0 && haystack[i] != needle[j] {
 			j = next[j - 1] // j失败，选取j-1的位置
 		}
+		// 相同，needle向后移动匹配下一个
 		if haystack[i] == needle[j] { j++ }
+		// 匹配上了，找到了，返回
 		if j == m { return i - m + 1 }
 	}
 
