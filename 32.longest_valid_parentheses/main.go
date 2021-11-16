9240570c0275ea2cedc653c2c23ac1fcba684da9
ranzishuo <ranzishuo@bytedance.com>
2021-11-16 14:35:42 +0800

    Updat

--- 32.longest_valid_parentheses/main.go
+++ 32.longest_valid_parentheses/main.go
@@ -3,38 +3,34 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(longestValidParentheses("()(()"))
+	fmt.Println(longestValidParentheses("()))()"))
 }
-// TODO 未完成
+
 func longestValidParentheses(s string) int {
 	// 如果出现多余右括号，直接更新最大连续数字，后重新计数
 	// 对于左括号，需要在最后结束的时候再统计是否匹配，所以需要把下标保存下来，
 	// 方便直接计算长度
-	left := 0
-	leftIdxs := make([]int, 0)
-	max, count := 0, 0
 
+	max := 0
+	stack := make([]int, 0)
+	// 添加一个-1作为栈底
+	stack = append(stack, -1)
 	for i := 0; i < len(s); i++ {
-		if s[i] == '(' { // 左括号
-			left++
-			leftIdxs = append(leftIdxs, i) // 计算最新一个左括号下标
-		} else { // 右括号
-			if left > 0 { // 正常情况，左括号够用
-				left--
-				leftIdxs = leftIdxs[:len(leftIdxs)-1]
-				count += 2
-			} else if max < count { // 特殊情况，右括号多余，立刻计算长度
-				max = count
-				count = 0 // 开始下一轮计数
+		if s[i] == '(' { // 左括号无脑加入
+			stack = append(stack, i)
+		} else {
+			// 遇到右括号先出栈一个左括号
+			if len(stack) > 0 { stack = stack[:len(stack)-1] }
+			if len(stack) == 0 {
+				// 出栈之后栈为空，则说明当前右括号不合法，入栈作为栈底
+				stack = append(stack, i)
+			} else {
+				// 当前右括号匹配上了，直接计算最大值
+				if max < i - stack[len(stack)-1] {
+					max = i - stack[len(stack)-1]
+				}
 			}
 		}
-	}
-
-	// 处理左括号
-	if left != 0 && len(s) - 1 - leftIdxs[len(leftIdxs)-1] > max {
-		max = len(s) - 1 - leftIdxs[len(leftIdxs)-1]
-	} else if max < count { // 刚好匹配，最后更一下最大值
-		max = count
 	}
 
 	return max
