8aa99971fdfd7f124641b08a742753c1a6714350
ranzishuo <ranzishuo@bytedance.com>
2021-11-16 18:16:28 +0800

    Update

--- 41.first_missing_positive/main.go
+++ 41.first_missing_positive/main.go
@@ -1,7 +1,18 @@
 package main
 
+//输入：nums = [1,2,0]
+//输出：3
 func firstMissingPositive(nums []int) int {
+	//给你一个未排序的整数数组 nums ，请你找出其中没有出现的最小的正整数。
+	//请你实现时间复杂度为 O(n) 并且只使用常数级别额外空间的解决方案。
 
+	// 简单来说就是记录这些正数字的范围
+	//min, max := 1, 1
+	//for _, v := range nums {
+	//	if v <= 0 {
+	//		continue
+	//	}
+	//}
 
 	return 0
 }
