ee637467518cfe02e446eaeed47825ec4bea2815
ranzishuo <ranzishuo@bytedance.com>
2021-11-16 12:50:07 +0800

    Update

---
+++ 40.combination_sum_2/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+//[2,5,2,1,2]
+//5
+
+func main() {
+	fmt.Println(combinationSum2([]int{2,5,2,1,2}, 5))
+}
+
+var res [][]int
+var can []int
+func combinationSum2(candidates []int, target int) [][]int {
+	res = make([][]int, 0)
+
+	// 使用map
+	m := make(map[int]int)
+	for _, v := range candidates {
+		m[v] = m[v] + 1
+	}
+
+	// 排序避免重复
+	sort.Ints(candidates)
+
+	fmt.Println(candidates)
+
+	can = candidates
+
+	recursion(len(candidates)-1, target, make([]int, 0), m)
+
+	return res
+}
+
+func recursion(idx int, tar int, list []int, m map[int]int) {
+	// 查找到一种情况，返回
+	if tar == 0 {
+		res = append(res, append(make([]int, 0), list...))
+		return
+	}
+
+	// 越界（无结果）或不是正好相等
+	if idx < 0 || tar < 0 {
+		return
+	}
+
+	// 如果尝试了m[can[idx]]这条路，并且can[idx] != can[idx-1]，就没必要试了
+	// 如果没有尝试
+	// 使用can[idx]
+
+	if m[can[idx]] > 0 {
+		m[can[idx]]--
+		list = append(list, can[idx])
+		recursion(idx, tar - can[idx], list, m)
+		m[can[idx]]++
+		list = list[:len(list)-1]
+	}
+
+	// 不使用can[idx]
+	if can[idx] != can[idx-1] {
+		recursion(idx - 1, tar, list, m)
+	}
+}
