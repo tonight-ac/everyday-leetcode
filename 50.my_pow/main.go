207aea982b47ebd6dbac2a65541cef9e8895a71e
tonight-ac <1531728675@qq.com>
2021-11-18 00:56:48 +0800

    Update

--- 50.my_pow/main.go
+++ 50.my_pow/main.go
@@ -1,5 +1,23 @@
 package main
 
+import "fmt"
+
+func main() {
+	fmt.Println(myPow(2.1000, 3))
+}
+
 func myPow(x float64, n int) float64 {
-	return 0.0
+	if n == 0 { return 1.0 }
+	res := x
+	if n > 0 {
+		for i := 0; i < n-1; i++ {
+			res *= x
+		}
+	} else {
+		for i := 0; i < -(n-1); i++ {
+			res /= x
+		}
+	}
+
+	return res
 }
