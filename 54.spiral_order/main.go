2d1f917fa36d613afbde7c544cfb64bdb4d87015
tonight-ac <1531728675@qq.com>
2021-11-19 00:34:55 +0800

    Update

--- 54.spiral_order/main.go
+++ 54.spiral_order/main.go
@@ -1,7 +1,7 @@
 package main
 
+import "fmt"
 
-// TODO 未通过
 //输入：
 //[
 //[1,2,3,4],
@@ -13,20 +13,29 @@ package main
 //[1,2,3,4,8,12,16,15,14,13,9,10,11,7,6,5]
 //预期结果：
 //[1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10]
+func main() {
+	fmt.Println(spiralOrder([][]int{{1,2,3},{4,5,6},{7,8,9}}))
+}
+
 var dir = [][]int{0: {0, 1}, 1: {1, 0}, 2: {0, -1}, 3: {-1, 0}}
 func spiralOrder(matrix [][]int) []int {
 	n := len(matrix)
 	m := len(matrix[0])
 	res := make([]int, 0, n*m)
 
+	// idx表示dir方向
 	j, k, idx := 0, 0, 0
-	for i := 0; i < cap(res); i++ {
+	// i表示结果集的第几位
+	for i := 0; ; i++ {
 		res = append(res, matrix[j][k])
+		if len(res) == n*m { break }
 		// matrix中的数字最大100，设置为101表示读取过了
 		matrix[j][k] = 101
 		// 按照右、下、左、上顺序更新j和k
-		for i := idx; idx < len(dir); i++ {
-			v := dir[idx]
+		for l := idx; ; l++ {
+			//fmt.Println(l%len(dir))
+			// 避免越界
+			v := dir[l%len(dir)]
 			if !(j + v[0] >= 0 && j + v[0] < n) {
 				continue
 			}
@@ -38,7 +47,7 @@ func spiralOrder(matrix [][]int) []int {
 			}
 			j += v[0]
 			k += v[1]
-			idx = i
+			idx = l
 			break
 		}
 	}
