b37874c41d9fb1ee19d60e64e51ec30da181b377
tonight-ac <1531728675@qq.com>
2021-11-18 22:12:16 +0800

    Update

--- 50.my_pow/main.go
+++ 50.my_pow/main.go
@@ -3,33 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(myPow(2.1000, 3))
+	fmt.Println(myPow(2.0, -2))
 }
 
 // 其实就是实现一个快速幂运算
 // 原理：x^22可以改写成x^16*x^4*x^2
 func myPow(x float64, n int) float64 {
+	if n == 0 { return 1.0 }
+
 	res := 1.0
-	if n > 0 {
-		for  n > 0 {
-			// 最低位存在的话，乘一个x
-			if n & 1 == 1 { res = res * x }
-			// 对x进行翻倍
-			x = x * x
-			// 吃掉最低位
-			n >>= 1
-		}
-	} else if n < 0 {
-		for  n < 0 {
-			// 最低位存在的话，乘一个x
-			if n & 1 == 1 { res = res / x }
-			x = x * x
-			n >>= 1 //相当于n=n/2.详情请参考位移运算符。
-		}
-	} else {
-		return 1.0
+	flag := false
+	if n < 0 {
+		n = -n
+		flag = true
+	}
+	for n > 0 {
+		// 最低位存在的话，乘一个x
+		if n & 1 == 1 { res = res * x }
+		// 对x进行翻倍
+		x = x * x
+		// 吃掉最低位
+		n >>= 1
 	}
 
+	if flag {
+		return 1.0 / res
+	}
 	return res
 }
 
