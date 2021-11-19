d8b15e26cad4237b40805ef78c137cfbcee951a0
ranzishuo <ranzishuo@bytedance.com>
2021-11-19 15:18:59 +0800

    Update

--- 57.insert/main.go
+++ 57.insert/main.go
@@ -1,6 +1,30 @@
 package main
 
+//输入：intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+//输出：[[1,2],[3,10],[12,16]]
+//解释：这是因为新的区间 [4,8] 与 [3,5],[6,7],[8,10]重叠。
+
 func insert(intervals [][]int, newInterval []int) [][]int {
+	// 两步走，先把newInterval根据newInterval[0]大小给塞进去，然后往两边扩展
+	// 寻找插入点
+	idx := len(intervals)
+	for i := 0; i < len(intervals); i++ {
+		if intervals[i][0] > newInterval[0] {
+			idx = i
+			break
+		}
+	}
+
+	left, right := idx, idx
+	// 左边向左寻找可能merge的interval
+	for left >= 0 {
+
+	}
+
+	// 右边向右寻找可能merge的interval
+	for right < len(intervals) {
+
+	}
 
 	return nil
 }
