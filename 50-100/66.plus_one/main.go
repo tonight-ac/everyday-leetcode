0834c47f364290d38cf2543829a3e2d6da942b57
ranzishuo <ranzishuo@bytedance.com>
2021-11-19 18:14:46 +0800

    Update

--- 50-100/66.plus_one/main.go
+++ 50-100/66.plus_one/main.go
@@ -1,6 +1,23 @@
 package main
 
 func plusOne(digits []int) []int {
+	carry := 1
+	for i := len(digits)-1; carry == 1 && i >= 0; i-- {
+		digits[i], carry = (digits[i]+carry)%10, (digits[i]+carry)/10
+	}
 
-	return nil
+	// 如果最后多进出来一位，需要插入在数列首位
+	if carry == 1 {
+		// 记录一下末尾数字，因为等下整体后移会把最后一位盖掉
+		end := digits[len(digits)-1]
+		for i := 1; i < len(digits); i++ {
+			digits[i] = digits[i-1]
+		}
+		// 添加进位
+		digits[0] = carry
+		// 把原来的末尾添加回来
+		digits = append(digits, end)
+	}
+
+	return digits
 }
