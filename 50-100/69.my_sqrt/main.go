bb2bc2a7b5e64d468cebe3d8036cedbdf8854f1d
ranzishuo <ranzishuo@bytedance.com>
2021-11-19 18:27:29 +0800

    Update

--- 50-100/69.my_sqrt/main.go
+++ 50-100/69.my_sqrt/main.go
@@ -1,7 +1,17 @@
 package main
 
-
+// 我理解就是50题的反向操作
 func mySqrt(x int) int {
+	res := 0
+	n := 2
+	for x > 0 {
+		// 最低位存在的话，乘一个x
+		if x & 1 == 1 { res = res / n }
+		// 对x进行翻倍
+		n = n * n
+		// 吃掉最低位
+		x >>= 1
+	}
 
 	return 0
 }
