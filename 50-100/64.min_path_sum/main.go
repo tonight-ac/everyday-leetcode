b84f24f778b472bdf3c3b268035d04f547d95817
ranzishuo <ranzishuo@bytedance.com>
2021-11-19 17:04:39 +0800

    Update

--- 50-100/64.min_path_sum/main.go
+++ 50-100/64.min_path_sum/main.go
@@ -1,6 +1,29 @@
 package main
 
+//输入：grid = [[1,3,1],[1,5,1],[4,2,1]]
+//输出：7
+//解释：因为路径 1→3→1→1→1 的总和最小。
 func minPathSum(grid [][]int) int {
+	m, n := len(grid), len(grid[0])
 
-	return 0
+	// 第一行和第一列初始化为1
+	for i := 1; i < m; i++ { grid[i][0] += grid[i-1][0] }
+	for j := 1; j < n; j++ { grid[0][j] += grid[0][j-1] }
+
+	// 开始DP
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			grid[i][j] += min(grid[i-1][j], grid[i][j-1])
+		}
+	}
+
+	return grid[m-1][n-1]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+
+	return b
 }
