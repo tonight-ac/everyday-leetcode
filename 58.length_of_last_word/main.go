ff67e45352d280e6d9b41d3f2d8a3baf44743de0
ranzishuo <ranzishuo@bytedance.com>
2021-11-19 15:22:55 +0800

    Update

--- 58.length_of_last_word/main.go
+++ 58.length_of_last_word/main.go
@@ -1,6 +1,24 @@
 package main
 
 func lengthOfLastWord(s string) int {
+	// 从后面开始遍历
+	i := len(s)-1
 
-	return 0
+	res := 0
+
+	// 排除所有后置空格
+	for ; i >= 0; i-- {
+		if s[i] != ' ' {
+			break
+		}
+	}
+
+	for ; i >= 0; i-- {
+		if s[i] == ' ' {
+			break
+		}
+		res++
+	}
+
+	return res
 }
