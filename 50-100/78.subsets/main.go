e5f7c3fa87aa8027705e038fb71f4e429abfdf87
tonight-ac <1531728675@qq.com>
2021-11-21 01:41:52 +0800

    Update

--- 50-100/78.subsets/main.go
+++ 50-100/78.subsets/main.go
@@ -1,6 +1,31 @@
 package main
 
-func subsets(nums []int) [][]int {
+import "fmt"
 
-	return nil
+func main() {
+	fmt.Println(subsets([]int{1,2,3}))
+}
+
+var res [][]int
+var n []int
+func subsets(nums []int) [][]int {
+	n = nums
+
+	res = make([][]int, 0)
+
+	recursion(make([]int, 0), 0)
+
+	return res
+}
+
+func recursion(s []int, idx int) {
+	if idx == len(n) {
+		res = append(res, append(make([]int, 0), s...))
+		return
+	}
+
+	// 不选择
+	recursion(s, idx+1)
+	// 选择
+	recursion(append(s, n[idx]), idx+1)
 }
