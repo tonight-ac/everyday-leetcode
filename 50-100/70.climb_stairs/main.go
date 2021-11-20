0d069815b008d0e0578a102634777372f848d389
tonight-ac <1531728675@qq.com>
2021-11-20 15:45:19 +0800

    Update

--- 50-100/70.climb_stairs/main.go
+++ 50-100/70.climb_stairs/main.go
@@ -1,6 +1,16 @@
 package main
 
+// 对于第n阶，只能通过n-1和n-2通过唯一的方法爬上来
+// 所以res[n]=res[n-1]+res[n-2]
 func climbStairs(n int) int {
+	if n <= 2 { return n }
 
-	return 0
+	res := make([]int, n+1)
+	res[1] = 1
+	res[2] = 2
+	for i := 3; i <= n; i++ {
+		res[i] = res[i-1]+res[i-2]
+	}
+
+	return res[n]
 }
