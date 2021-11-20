6fa3cd3e5a734fc9099e201e79c2201ce45574d9
tonight-ac <1531728675@qq.com>
2021-11-20 20:52:15 +0800

    Update

--- 50-100/69.my_sqrt/main.go
+++ 50-100/69.my_sqrt/main.go
@@ -1,21 +1,34 @@
 package main
 
-// TODO 未完成
-// 我理解就是50题的反向操作
+// 使用二分法试一下提升速度
 func mySqrt(x int) int {
-	res := 0
-	n := 2
-	//count := 1
-	for x > 0 {
-		// 最低位存在的话，乘一个x
-		if x & 1 == 1 {
-			x = x / n
+	l, r, mid := 0, x, 0
+
+	for l <= r {
+		mid = (l + r) >> 1
+		if x >= mid*mid && x < (mid+1)*(mid+1) {
+			return mid
+		} else if x < mid*mid {
+			r = mid - 1
+		} else {
+			l = mid + 1
 		}
-		// 对x进行翻倍
-		n = n * n
-		// 吃掉最低位
-		x >>= 1
 	}
 
-	return res
+	return 0
 }
+
+// 可以利用50题的思路，但实际上50题只有整数，没有小数操作
+// x=res^2 求x
+// 因为答案是整数，所以必然存在res^2<=x<(res+1)^2
+// 暴力法如下，可以通过，但是速度很慢
+//func mySqrt(x int) int {
+//	if x == 0 { return 0 }
+//	for i := 2; i <= x/2; i++ {
+//		if x < (i+1)*(i+1) {
+//			return i
+//		}
+//	}
+//
+//	return 1
+//}
