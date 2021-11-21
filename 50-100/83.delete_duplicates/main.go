c47a9f126afdfb84cc5c421e6d7ba1e4d4da4a4d
tonight-ac <1531728675@qq.com>
2021-11-21 19:06:38 +0800

    Update

--- 50-100/83.delete_duplicates/main.go
+++ 50-100/83.delete_duplicates/main.go
@@ -5,18 +5,16 @@ type ListNode struct {
 	Next *ListNode
 }
 
-// 如果下一个节点跟当前数字相同，下一节节点删除
+// 如果下一个节点跟当前数字相同，下一个节点删除
 func deleteDuplicates(head *ListNode) *ListNode {
 	if head == nil || head.Next == nil { return head }
 
-	for cur, next := head, head.Next; cur != nil && next != nil; {
+	for cur := head; cur != nil && cur.Next != nil; {
 		// 相同则删除next
-		if cur.Val == next.Val {
-			cur.Next = next.Next
-			next = cur.Next
+		if cur.Val == cur.Next.Val {
+			cur.Next = cur.Next.Next
 		} else {
 			cur = cur.Next
-			next = next.Next
 		}
 	}
 
