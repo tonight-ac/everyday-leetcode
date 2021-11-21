f523a35720b8247339d3b99c5ff24256dbb44618
tonight-ac <1531728675@qq.com>
2021-11-21 17:17:37 +0800

    Update

--- 50-100/79.exist/main.go
+++ 50-100/79.exist/main.go
@@ -1,5 +1,6 @@
 package main
 
+// TODO 未完成
 func exist(board [][]byte, word string) bool {
 
 
--- 50-100/80.remove_duplicates/main.go
+++ 50-100/80.remove_duplicates/main.go
@@ -1,6 +1,28 @@
 package main
 
-func removeDuplicates(nums []int) int {
+import "fmt"
 
-	return 0
+func main()  {
+	nums := []int{0,0,1,1,1,1,2,3,3}
+	fmt.Println(removeDuplicates(nums))
+	fmt.Println(nums)
 }
+//输入：nums = [1,1,1,2,2,3]
+//输出：5, nums = [1,1,2,2,3]
+//
+func removeDuplicates(nums []int) int {
+	// 本来就有序长度又小于2，直接返回
+	if len(nums) == 1 { return 1 }
+	res := 2
+	for i := 2; i < len(nums); i++ {
+		// i表示待插入元素，res表示插入位置，i不能和上一个和上两个插入的相同
+		// 简化一下，由于有序性，nums[res-2]必然小于nums[res-1]，所以只判断nums[res-2]足够
+		if nums[i] != nums[res-2] {
+			nums[res] = nums[i]
+			res++
+		}
+	}
+
+	return res
+}
+
