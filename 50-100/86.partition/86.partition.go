78bd28b724402b6a4e51b5f5ff49150e0d2e952f
tonight-ac <1531728675@qq.com>
2021-11-21 20:19:42 +0800

    Update

--- 50-100/86.partition/86.partition.go
+++ 50-100/86.partition/86.partition.go
@@ -5,7 +5,47 @@ type ListNode struct {
 	Next *ListNode
 }
 
-func partition(head *ListNode, x int) *ListNode {
+func main() {
+	list := []int{1}
+	head := &ListNode{
+		Val: 1,
+	}
 
-	return nil
+	for temp, i := head, 1; i < len(list); i++ {
+		temp.Next = &ListNode{Val: list[i]}
+		temp = temp.Next
+	}
+	partition(head, 0)
+}
+
+// 小于x的放一列，大于等于x的放另外一列
+// 然后再把两列拼接在一起
+func partition(head *ListNode, x int) *ListNode {
+	sHead, lHead := &ListNode{}, &ListNode{}
+	sEnd, lEnd := sHead, lHead
+
+	for ; head != nil; {
+		if head.Val < x { // 小的放进s
+			sEnd.Next = head
+			sEnd = sEnd.Next
+			head = head.Next
+			sEnd.Next = nil
+		} else { // 大的放进l
+			lEnd.Next = head
+			lEnd = lEnd.Next
+			head = head.Next
+			lEnd.Next = nil
+		}
+	}
+
+	// s没有的话直接返回l
+	if sHead.Next == nil {
+		return lHead.Next
+	}
+
+	// 把l拼接到s后面
+	sEnd.Next = lHead.Next
+
+	// 返回s
+	return sHead.Next
 }
