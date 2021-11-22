160a22a509ee46adb10792fade95172713d076b7
ranzishuo <ranzishuo@bytedance.com>
2021-11-22 18:42:18 +0800

    Update

--- 50-100/92.reverse_between/main.go
+++ 50-100/92.reverse_between/main.go
@@ -5,7 +5,9 @@ type ListNode struct {
 	Next *ListNode
 }
 
+// 需要用到19的指针和206题的反转数组
 func reverseBetween(head *ListNode, left int, right int) *ListNode {
+	if left == right { return head }
 
 	return nil
 }
