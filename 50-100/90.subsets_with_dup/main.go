c3dd2ca6a69661f66e48c1e67c4a99d393aa9c90
ranzishuo <ranzishuo@bytedance.com>
2021-11-22 11:09:58 +0800

    Update

--- 50-100/90.subsets_with_dup/main.go
+++ 50-100/90.subsets_with_dup/main.go
@@ -1,19 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
-	fmt.Println(subsetsWithDup([]int{1,2,2}))
+	fmt.Println(subsetsWithDup([]int{4,4,4,1,4}))
 }
 // TODO 未完成
 // 类似78题，但存在递进
 // 和78题的递进关系类似46题和47题
 var res [][]int
 var n []int
+var uni map[int]bool
 func subsetsWithDup(nums []int) [][]int {
 	n = nums
 
 	res = make([][]int, 0)
+
+	uni = make(map[int]bool)
+
+	// 先排个顺序
+	sort.Ints(nums)
 
 	recursion(make([]int, 0), 0)
 
@@ -26,15 +35,14 @@ func recursion(s []int, idx int) {
 		return
 	}
 
-	// 当前等于之前的，直接忽略掉
-	if idx == 0 || n[idx] != n[idx-1] {
-		// 选择
-		recursion(append(s, n[idx]), idx+1)
-	} else if len(s) > 0 && n[idx] == s[len(s)-1] {
-		// 选择
-		recursion(append(s, n[idx]), idx+1)
+	// 如果已经存在了，则必须选择当前元素
+	// 例如[0]2,[1]2,[2]2
+	if !uni[n[idx]] {
+		// 不选择
+		recursion(s, idx+1)
 	}
 
-	// 不选择
-	recursion(s, idx+1)
+	uni[n[idx]] = true
+	recursion(append(s, n[idx]), idx+1)
+	uni[n[idx]] = false
 }
