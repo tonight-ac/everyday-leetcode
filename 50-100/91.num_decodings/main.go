3d03118b3a7982a56027decb8f4a7049dc2a8c42
tonight-ac <1531728675@qq.com>
2021-11-22 21:45:49 +0800

    Update

--- 50-100/91.num_decodings/main.go
+++ 50-100/91.num_decodings/main.go
@@ -5,9 +5,12 @@ import "fmt"
 func main() {
 	fmt.Println(numDecodings("1123"))
 }
-// TODO 未完成
+// 1123 1 1 2 3 \ 11 2 3 \ 11 23 \ 1 12 3 \ 1 1 23
+// 1 2 3 4
+
 // 换一个dp的方式
 func numDecodings(s string) int {
+	// dp数组，dp[i]表示，以s[i]为结尾的字符字串s[0:i]有多少种解码方式
 	dp := make([]int, len(s))
 
 	// 含有导0，不合法，返回0
@@ -18,13 +21,13 @@ func numDecodings(s string) int {
 
 	for i := 1; i < len(s); i++ {
 		if s[i] >= '1' && s[i] <= '9' { // 如果当前合法
-			if s[i-1] != '0' && (s[i-1]-'0')*10+(s[i]-'0') <= 26 { // 一位和两位都合法，+2
-				if i >= 2 {
-					dp[i] = dp[i-2] + 2
-				} else {
+			if s[i-1] != '0' && (s[i-1]-'0')*10+(s[i]-'0') <= 26 { // 一位和两位都合法
+				if i >= 2 { // 将排除一位和两位的情况相加
+					dp[i] = dp[i-2] + dp[i-1]
+				} else { // 只有两位直接等于2
 					dp[i] = 2
 				}
-			} else { // 两位不合法
+			} else { // 不存在两位的情况，相当于单独加一种唯一情况
 				dp[i] = dp[i-1]
 			}
 		} else { // 当前不合法
