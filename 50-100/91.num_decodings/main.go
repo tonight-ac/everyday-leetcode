c18e9914ed0c0139827084eb97f7927295e6a6a5
ranzishuo <ranzishuo@bytedance.com>
2021-11-22 19:45:42 +0800

    Update

--- 50-100/91.num_decodings/main.go
+++ 50-100/91.num_decodings/main.go
@@ -3,20 +3,44 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(numDecodings("226"))
+	fmt.Println(numDecodings("1123"))
 }
 
 // 换一个dp的方式
 func numDecodings(s string) int {
-	list := make([]int, len(s))
+	dp := make([]int, len(s))
 
-	for i := 0; i < len(s); i++ {
-		if i>=1 && s[i] >= '1' && s[i] <= '9' { list[i] += list[i-1] + 1 }
-		if i>=2 && s[i] != '0' && (s[i-1]-'0')*10+(s[i]-'0') <= 26 { list[i] += list[i-2] + 1 }
-		if list[i] == 0 { break }
+	// 含有导0，不合法，返回0
+	if s[0] == '0' { return 0 }
+
+	// 合法，置第一个为1
+	dp[0] = 1
+
+	for i := 1; i < len(s); i++ {
+		if s[i] >= '1' && s[i] <= '9' { // 如果当前合法
+			if s[i-1] != '0' && (s[i-1]-'0')*10+(s[i]-'0') <= 26 { // 一位和两位都合法，+2
+				if i >= 2 {
+					dp[i] = dp[i-2] + 2
+				} else {
+					dp[i] = 2
+				}
+			} else { // 两位不合法
+				dp[i] = dp[i-1]
+			}
+		} else { // 当前不合法
+			if s[i-1] != '0' && (s[i-1]-'0')*10+(s[i]-'0') <= 26 { // 和前一位组成的两位合法
+				if i >= 2 {
+					dp[i] = dp[i-2]
+				} else {
+					dp[i] = 1
+				}
+			} else { // 一位和两位都不合法
+				break
+			}
+		}
 	}
 
-	return list[len(s)-1]
+	return dp[len(s)-1]
 }
 
 // 递归解决，递归超时了
