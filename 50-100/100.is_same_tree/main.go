5bb48b61540d6e3709fc80f41057ddb913930621
ranzishuo <ranzishuo@bytedance.com>
2021-11-22 18:25:28 +0800

    Update

--- 50-100/100.is_same_tree/main.go
+++ 50-100/100.is_same_tree/main.go
@@ -6,8 +6,13 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+// 递归解决
 func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	} else if p != nil && q != nil {
+		return p.Val == q.Val && isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+	}
 
 	return false
 }
-
