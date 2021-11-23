eccfb725f3ff154fcee99b9916168a27ac756d22
ranzishuo <ranzishuo@bytedance.com>
2021-11-23 17:21:45 +0800

    Update

--- 50-100/97.is_interleave/main.go
+++ 50-100/97.is_interleave/main.go
@@ -8,22 +8,59 @@ func main() {
 	fmt.Println(isInterleave("aabcc", "dbbca", "aadbcbbcac"))
 }
 
-// 改dp试一下
+var temp []string
+// 先对于更短的串和混合串进行匹配拆解，将空出来的所有可能拼接然后和长串进行比较
 func isInterleave(s1 string, s2 string, s3 string) bool {
-	dp := make([][]int, len(s1))
-	for i := 0; i < len(dp); i++ { dp[i] = make([]int, len(s2)) }
+	if len(s1) + len(s2) != len(s3) { return false }
 
+	// s1作为更短的串，s2作为更长的串
+	if len(s1) > len(s2) {
+		s1, s2 = s2, s1
+	}
 
+	// 将所有可能和s2进行比较
+	for _, v := range temp {
+		if v == s2 {
+			return true
+		}
+	}
 
 	return false
 }
 
-func abs(a int) int {
-	if a < 0 {
-		return -a
-	}
-	return a
-}
+//func recursion(s1, s3 string, s []string) {
+//	start := 0
+//	for i, j := 0, 0; i < len(s1) && j < len(s3); {
+//		if s1[i] != s3[j] {
+//			start = j+1
+//		} else {
+//			// 把s1匹配的部分切掉
+//			//recursion(s1[:i], )
+//			i++
+//			j++
+//		}
+//		//if ok := recursion(s1[i+1:], s2, s3[i+1:], left+1, right); ok {
+//		//	return true
+//		//}
+//	}
+//}
+
+// 改dp试一下
+//func isInterleave(s1 string, s2 string, s3 string) bool {
+//	dp := make([][]int, len(s1))
+//	for i := 0; i < len(dp); i++ { dp[i] = make([]int, len(s2)) }
+//
+//
+//
+//	return false
+//}
+//
+//func abs(a int) int {
+//	if a < 0 {
+//		return -a
+//	}
+//	return a
+//}
 
 // 大力没有出奇迹，而是超时了
 //"aabcc"
