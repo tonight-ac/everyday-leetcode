866c1fbcfca8aef8053f6183e3e38aacc5ef5c45
tonight-ac <1531728675@qq.com>
2021-11-23 23:28:14 +0800

    Update

--- 50-100/95.generate_trees/main.go
+++ 50-100/95.generate_trees/main.go
@@ -15,41 +15,46 @@ func main() {
 	fmt.Println(generateTrees(3))
 }
 
-//  1 2 3
+// 1 <= n <= 8
+// 先完成遍历
+var nums = []int{1,2,3,4,5,6,7,8}
+var uni [9]int
 var nn int
-var uni []int
-var res []*TreeNode
 func generateTrees(n int) []*TreeNode {
 	nn = n
 
-	uni = make([]int, nn+1)
+	uni = [9]int{}
 
-	recursion(nil, &TreeNode{})
-
-	return res
+	return nil
 }
 
-func recursion(n *TreeNode, head *TreeNode) {
-	if len(uni) == nn {
-		res = append(res, head)
-		return
-	}
-	for i := 1; i <= nn; i++ {
-		if uni[i] != 1 {
-			uni[i] = 1
-			if head.Val == 0 {
-				head.Val = i
-				recursion(head, head)
-			} else if i < n.Val {
-				n.Left = &TreeNode{ Val: i }
-				recursion(n.Left, head)
-				n.Left = nil
-			} else {
-				n.Right = &TreeNode{ Val: i }
-				recursion(n.Right, head)
-				n.Right = nil
-			}
-			uni[i] = 0
+func recursion(head *TreeNode, left, right []int) []*TreeNode {
+	lefts := make([]*TreeNode, 0)
+	for i := 0; i < len(left); i++ {
+		temp := &TreeNode{
+			Val: head.Val,
+			Left: &TreeNode{
+				Val: left[i],
+			},
 		}
+		lefts = append(lefts, recursion(temp.Left, left[:i], left[i+1:])...)
 	}
+
+	rights := make([]*TreeNode, 0)
+	for i := 0; i < len(right); i++ {
+		temp := &TreeNode{
+			Val: head.Val,
+			Right: &TreeNode{
+				Val: right[i],
+			},
+		}
+		rights = append(rights, recursion(temp.Right, right[:i], right[i+1:])...)
+	}
+
+	return nil
+}
+
+// 树粘贴
+func copyTrees(src *TreeNode, dst *TreeNode) {
+
 }
