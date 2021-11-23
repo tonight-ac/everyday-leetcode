ad352818ec60d0ff8e66dcde6b3e46b50d9c6909
ranzishuo <ranzishuo@bytedance.com>
2021-11-23 12:41:48 +0800

    Update

--- 50-100/97.is_interleave/main.go
+++ 50-100/97.is_interleave/main.go
@@ -1,10 +1,16 @@
 package main
 
 import "fmt"
-
+//"cacbbbaaabbacbbbbabbcaccccabaaacacbcaacababbaabbaccacbaabac"
+//"cbcccabbbbaaacbaccbabaabbccbbbabacbaacccbbcaabaabbbcbcbab"
+//"ccbcccacbabbbbbbaaaaabbaaccbabbbbacbcbcbaacccbacabbaccbaaabcacbbcabaabacbbcaacaccbbacaabababaabbbaccbbcacbbacabbaacb"
 func main() {
 	fmt.Println(isInterleave("aabcc", "dbbca", "aadbcbbcac"))
 }
+// 1 aa
+// 2    d
+// 3 bc
+// 4
 
 // 大力没有出奇迹，而是超时了
 //"aabcc"
@@ -17,23 +23,23 @@ func isInterleave(s1 string, s2 string, s3 string) bool {
 }
 
 func recursion(s1 string, s2 string, s3 string, left, right int) bool {
-	if abs(left-right) > 1 {
-		return false
-	}
-
 	if len(s1) + len(s2) + len(s3) == 0 {
 		return abs(left-right) <= 1
 	}
 
-	for i := 0; i < len(s1) && i < len(s3) && s1[i] == s3[i]; i++ {
-		if ok := recursion(s1[i+1:], s2, s3[i+1:], left+1, right); ok {
-			return true
+	if left <= right+1 {
+		for i := 0; i < len(s1) && i < len(s3) && s1[i] == s3[i]; i++ {
+			if ok := recursion(s1[i+1:], s2, s3[i+1:], left+1, right); ok {
+				return true
+			}
 		}
 	}
 
-	for i := 0; i < len(s2) && i < len(s3) && s2[i] == s3[i]; i++ {
-		if ok := recursion(s1, s2[i+1:], s3[i+1:], left, right+1); ok {
-			return true
+	if right <= left+1 {
+		for i := 0; i < len(s2) && i < len(s3) && s2[i] == s3[i]; i++ {
+			if ok := recursion(s1, s2[i+1:], s3[i+1:], left, right+1); ok {
+				return true
+			}
 		}
 	}
 
