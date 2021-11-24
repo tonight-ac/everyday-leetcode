ac13a2a25737733831c21619b9ec8244bb244eea
ranzishuo <ranzishuo@bytedance.com>
2021-11-24 17:34:57 +0800

    Update

--- 50-100/97.is_interleave/main.go
+++ 50-100/97.is_interleave/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 //"abbbbbbcabbacaacccababaabcccabcacbcaabbbacccaaaaaababbbacbb"
 //"ccaacabbacaccacababbbbabbcacccacccccaabaababacbbacabbbbabc"
@@ -21,64 +20,84 @@ func main() {
 	//fmt.Println(isInterleave("aabcc", "dbbca", "aadbbcbcac"))
 }
 
-var temp []string
-var ss string
-// 先对于更短的串和混合串进行匹配拆解，将空出来的所有可能拼接然后和长串进行比较
 func isInterleave(s1 string, s2 string, s3 string) bool {
-	if len(s1) + len(s2) != len(s3) { return false }
+	n, m, t := len(s1), len(s2), len(s3)
+	if (n + m) != t { return false }
 
-	// s1作为更短的串，s2作为更长的串
-	if len(s1) > len(s2) {
-		s1, s2 = s2, s1
-	}
-
-	temp = make([]string, 0)
-
-	ss = s2
-
-	recursion(s1, s3, make([]string, 0))
-
-	//fmt.Println(temp)
-
-	// 将所有可能和s2进行比较
-	for _, v := range temp {
-		if v == s2 {
-			return true
+	f := make([]bool, m + 1)
+	f[0] = true
+	for i := 0; i <= n; i++ {
+		for j := 0; j <= m; j++ {
+			p := i + j - 1
+			if i > 0 {
+				f[j] = f[j] && s1[i-1] == s3[p]
+			}
+			if j > 0 {
+				f[j] = f[j] || f[j-1] && s2[j-1] == s3[p]
+			}
 		}
 	}
-
-	return false
+	return f[m]
 }
 
-func recursion(s1, s3 string, s []string) {
-	// s1匹配完毕了，把结果保存后返回
-	if len(s1) == 0 {
-		temp = append(temp, strings.Join(s, "")+s3)
-		return
-	}
-
-	// s3也为空了，返回，不然会越界
-	if len(s3) == 0 {
-		return
-	}
-
-	// 关键性剪枝操作
-	if !strings.HasPrefix(ss, strings.Join(s, "")) {
-		return
-	}
-
-	if s1[0] == s3[0] {
-		// 相等并匹配
-		recursion(s1[1:], s3[1:], s)
-		// 相等但不让s1匹配，跳过
-		if len(s) != len(ss) && ss[len(s)] == s3[0] { //
-			recursion(s1, s3[1:], append(s, s3[0:1]))
-		}
-	} else {
-		// 不相等，直接跳过
-		recursion(s1, s3[1:], append(s, s3[0:1]))
-	}
-}
+//var temp []string
+//var ss string
+//// 先对于更短的串和混合串进行匹配拆解，将空出来的所有可能拼接然后和长串进行比较
+//func isInterleave(s1 string, s2 string, s3 string) bool {
+//	if len(s1) + len(s2) != len(s3) { return false }
+//
+//	// s1作为更短的串，s2作为更长的串
+//	if len(s1) > len(s2) {
+//		s1, s2 = s2, s1
+//	}
+//
+//	temp = make([]string, 0)
+//
+//	ss = s2
+//
+//	recursion(s1, s3, make([]string, 0))
+//
+//	//fmt.Println(temp)
+//
+//	// 将所有可能和s2进行比较
+//	for _, v := range temp {
+//		if v == s2 {
+//			return true
+//		}
+//	}
+//
+//	return false
+//}
+//
+//func recursion(s1, s3 string, s []string) {
+//	// s1匹配完毕了，把结果保存后返回
+//	if len(s1) == 0 {
+//		temp = append(temp, strings.Join(s, "")+s3)
+//		return
+//	}
+//
+//	// s3也为空了，返回，不然会越界
+//	if len(s3) == 0 {
+//		return
+//	}
+//
+//	// 关键性剪枝操作
+//	if !strings.HasPrefix(ss, strings.Join(s, "")) {
+//		return
+//	}
+//
+//	if s1[0] == s3[0] {
+//		// 相等并匹配
+//		recursion(s1[1:], s3[1:], s)
+//		// 相等但不让s1匹配，跳过
+//		if len(s) != len(ss) && ss[len(s)] == s3[0] { //
+//			recursion(s1, s3[1:], append(s, s3[0:1]))
+//		}
+//	} else {
+//		// 不相等，直接跳过
+//		recursion(s1, s3[1:], append(s, s3[0:1]))
+//	}
+//}
 
 // 改dp试一下
 //func isInterleave(s1 string, s2 string, s3 string) bool {
