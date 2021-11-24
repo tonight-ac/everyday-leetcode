2dc5f23cfe83c4bf6be5fd31481c4788a9d6f8f6
ranzishuo <ranzishuo@bytedance.com>
2021-11-24 12:58:57 +0800

    Update

--- 50-100/99.recover_tree/main.go
+++ 50-100/99.recover_tree/main.go
@@ -1,11 +1,60 @@
 package main
 
+import "fmt"
+
 type TreeNode struct {
 	Val int
 	Left *TreeNode
 	Right *TreeNode
 }
 
+func main() {
+	root := &TreeNode{
+		Val: 2,
+		Left: &TreeNode{
+			Val: 3,
+		},
+		Right: &TreeNode{
+			Val: 1,
+		},
+	}
+	recoverTree(root)
+	fmt.Println(1)
+}
+
+// 借鉴98题
 func recoverTree(root *TreeNode)  {
+	recursion(root, nil, nil)
+}
+
+// 左右两个数值判断
+func recursion(n, left, right *TreeNode) {
+	if n == nil { return }
+	if left != nil {
+		if n.Val < left.Val {
+			n.Val, left.Val = left.Val, n.Val
+			//return
+		}
+	}
+	recursion(n.Left, left, n)
+	if left != nil {
+		if n.Val < left.Val {
+			n.Val, left.Val = left.Val, n.Val
+			//return
+		}
+	}
+	if right != nil {
+		if n.Val > right.Val {
+			n.Val, right.Val = right.Val, n.Val
+			//return
+		}
+	}
+	recursion(n.Right, n, right)
+	if right != nil {
+		if n.Val > right.Val {
+			n.Val, right.Val = right.Val, n.Val
+			//return
+		}
+	}
 
 }
