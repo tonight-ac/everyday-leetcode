aa248cf3d398a6d88c47a58c803a4a7d70d0d719
tonight-ac <1531728675@qq.com>
2021-11-28 01:33:10 +0800

    Update

--- 50-100/87.is_scramble/main.go
+++ 50-100/87.is_scramble/main.go
@@ -32,23 +32,56 @@ import "fmt"
 // 自顶向下搜索是不可以的
 
 func main() {
-	fmt.Println(isScramble("abc", "cba"))
+	fmt.Println(isScramble("abcdbdacbdac", "bdacabcdbdac"))
 }
 
+// 用一个栈解决，借鉴了84的思路
+// 不是所有题都需要用dp，很多时候向前搜索都需要用到栈
+//"abcdbdacbdac"
+//"bdacabcdbdac"
 func isScramble(s1 string, s2 string) bool {
-	if s1 == s2 { return true }
-
-	// 0 1 2 3 4 5
-	for i := 1; i < len(s1); i++ {
-		// 不交换
-		if ok := isScramble(s1[:i], s2[:i]) && isScramble(s1[i:], s2[i:]); ok {
-			return true
-		}
-		// 交换
-		if ok := isScramble(s1[i:], s2[:len(s2)-i]) && isScramble(s1[:i], s2[len(s2)-i:]); ok {
-			return true
+	stack := make([]string, 0)
+	i, j := 0, 0
+	for i < len(s1) && j < len(s2) {
+		if s1[i] == s2[j] {
+			i++
+			j++
+		} else if len(stack) != 0 && stack[len(stack)-1] == string(rune(s2[j])) {
+			// 匹配出栈
+			stack = stack[:len(stack)-1]
+			j++
+		} else {
+			stack = append(stack, string(rune(s1[i])))
+			i++
 		}
 	}
 
-	return false
+	for len(stack) != 0 && j < len(s2) {
+		if stack[len(stack)-1] == string(rune(s2[j])) {
+			stack = stack[:len(stack)-1]
+			j++
+		} else {
+			return false
+		}
+	}
+
+	return len(stack) == 0 && j == len(s2)
 }
+
+//func isScramble(s1 string, s2 string) bool {
+//	if s1 == s2 { return true }
+//
+//	// 0 1 2 3 4 5
+//	for i := 1; i < len(s1); i++ {
+//		// 不交换
+//		if ok := isScramble(s1[:i], s2[:i]) && isScramble(s1[i:], s2[i:]); ok {
+//			return true
+//		}
+//		// 交换
+//		if ok := isScramble(s1[i:], s2[:len(s2)-i]) && isScramble(s1[:i], s2[len(s2)-i:]); ok {
+//			return true
+//		}
+//	}
+//
+//	return false
+//}
