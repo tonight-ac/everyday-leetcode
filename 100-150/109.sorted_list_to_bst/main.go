d556a1886d57cd305a1b480b6eb0af97bd19c51c
tonight-ac <1531728675@qq.com>
2021-11-29 02:10:05 +0800

    Update

--- 100-150/109.sorted_list_to_bst/main.go
+++ 100-150/109.sorted_list_to_bst/main.go
@@ -11,7 +11,16 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+// TODO
+// 借鉴108 通过快慢指针，判断中间位置
 func sortedListToBST(head *ListNode) *TreeNode {
-
+	if head == nil { return nil }
+	if head.Next == nil { return &TreeNode{ Val: head.Val }}
+	slow, quick := head, head
+	// 1 2 3 4 5
+	for quick != nil && quick.Next != nil {
+		slow = slow.Next
+		quick = quick.Next.Next
+	}
 	return nil
 }
