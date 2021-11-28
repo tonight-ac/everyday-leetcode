c100b2229fdf74350639a8aa588abaf11c40b69b
tonight-ac <1531728675@qq.com>
2021-11-29 00:49:03 +0800

    Update

--- 100-150/101.is_symmetric/main.go
+++ 100-150/101.is_symmetric/main.go
@@ -7,6 +7,18 @@ type TreeNode struct {
 }
 
 func isSymmetric(root *TreeNode) bool {
+	if root == nil { return false }
+	return recursion(root.Left, root.Right)
+}
 
-	return false
+func recursion(l, r *TreeNode) bool {
+	if l == nil && r == nil {
+		return true
+	} else if l != nil && r != nil {
+		if l.Val != r.Val { return false }
+	} else {
+		return false
+	}
+
+	return recursion(l.Left, r.Right)  && recursion(l.Right, r.Left)
 }
