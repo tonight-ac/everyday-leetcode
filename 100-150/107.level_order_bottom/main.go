f0b7661bb893cac5786134fad27c1140130c551e
tonight-ac <1531728675@qq.com>
2021-11-29 02:05:46 +0800

    Update

--- 100-150/107.level_order_bottom/main.go
+++ 100-150/107.level_order_bottom/main.go
@@ -6,7 +6,33 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+// 借鉴102
+var res [][]int
 func levelOrderBottom(root *TreeNode) [][]int {
+	res = make([][]int, 0)
 
-	return nil
+	if root == nil { return res }
+
+	recursion(root, 0)
+
+	// 交换res
+	for i, j := 0, len(res)-1; i < j; i, j = i+1, j-1 {
+		res[i], res[j] = res[j], res[i]
+	}
+
+	return res
+}
+
+func recursion(n *TreeNode, depth int) {
+	// 给res动态扩容
+	for len(res) <= depth {
+		res = append(res, make([]int, 0))
+	}
+	res[depth] = append(res[depth], n.Val)
+	if n.Left != nil {
+		recursion(n.Left, depth+1)
+	}
+	if n.Right != nil {
+		recursion(n.Right, depth+1)
+	}
 }
