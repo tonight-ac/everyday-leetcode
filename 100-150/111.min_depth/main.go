406bc612c0d15f0239cf6b937482555da3073b54
tonight-ac <1531728675@qq.com>
2021-11-29 01:21:51 +0800

    Update

--- 100-150/111.min_depth/main.go
+++ 100-150/111.min_depth/main.go
@@ -7,6 +7,23 @@ type TreeNode struct {
 }
 
 func minDepth(root *TreeNode) int {
+	if root == nil { return 0 }
+	if root.Left == nil && root.Right == nil {
+		return 1
+	}
+	if root.Right == nil {
+		return minDepth(root.Left)+1
+	}
+	if root.Left == nil {
+		return minDepth(root.Right)+1
+	}
+	return min(minDepth(root.Left), minDepth(root.Right))+1
+}
 
-	return 0
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+
+	return b
 }
