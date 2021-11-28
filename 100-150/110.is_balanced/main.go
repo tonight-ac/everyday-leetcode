b013f5d6d6290806df5001f00cefd8ed1f769ccd
tonight-ac <1531728675@qq.com>
2021-11-29 01:27:34 +0800

    Update

--- 100-150/110.is_balanced/main.go
+++ 100-150/110.is_balanced/main.go
@@ -5,8 +5,31 @@ type TreeNode struct {
 	Left *TreeNode
 	Right *TreeNode
 }
-
+// TODO 未完成
+// 超时了
+// 借用104
+var balanced bool
 func isBalanced(root *TreeNode) bool {
+	balanced = true
 
-	return false
+	recursion(root)
+
+	return balanced
+}
+
+func recursion(root *TreeNode) int {
+	if root == nil { return 0 }
+	if abs(recursion(root.Left) - recursion(root.Right)) > 1 { balanced = false }
+	return max(recursion(root.Left), recursion(root.Right))+1
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+func max(a, b int) int {
+	if a > b { return a }
+	return b
 }
