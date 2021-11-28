d8121c29c47c7ec1ca61e0c25e568c52a606084e
tonight-ac <1531728675@qq.com>
2021-11-29 00:55:39 +0800

    Update

--- 100-150/102.level_order/main.go
+++ 100-150/102.level_order/main.go
@@ -6,7 +6,27 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+var res [][]int
 func levelOrder(root *TreeNode) [][]int {
+	res = make([][]int, 0)
 
-	return nil
+	if root == nil { return res }
+
+	recursion(root, 0)
+
+	return res
+}
+
+func recursion(n *TreeNode, depth int) {
+	// 给res动态扩容
+	for len(res) <= depth {
+		res = append(res, make([]int, 0))
+	}
+	res[depth] = append(res[depth], n.Val)
+	if n.Left != nil {
+		recursion(n.Left, depth+1)
+	}
+	if n.Right != nil {
+		recursion(n.Right, depth+1)
+	}
 }
