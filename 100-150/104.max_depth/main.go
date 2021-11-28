02d6fce6f6847baf57f6b779933a81efd3169e2f
tonight-ac <1531728675@qq.com>
2021-11-29 01:05:02 +0800

    Update

--- 100-150/104.max_depth/main.go
+++ 100-150/104.max_depth/main.go
@@ -6,7 +6,24 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+var res int
 func maxDepth(root *TreeNode) int {
+	res = 0
 
-	return 0
+	if root == nil { return res }
+
+	recursion(root, 1)
+
+	return res
+}
+
+func recursion(root *TreeNode, depth int) {
+	if res < depth { res = depth }
+
+	if root.Left != nil {
+		recursion(root.Left, depth+1)
+	}
+	if root.Right != nil {
+		recursion(root.Right, depth+1)
+	}
 }
