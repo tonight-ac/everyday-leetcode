f9882b72e23af3eafb1278dfb35adbb4a909910e
ranzishuo <ranzishuo@bytedance.com>
2021-11-29 13:06:36 +0800

    Update

--- 100-150/112.has_path_sum/main.go
+++ 100-150/112.has_path_sum/main.go
@@ -6,7 +6,12 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+// TODO
 func hasPathSum(root *TreeNode, targetSum int) bool {
-
+	if root == nil { return false }
+	if targetSum == 0 { return true }
+	if targetSum >= root.Val {
+		return hasPathSum(root.Left, targetSum-root.Val) || hasPathSum(root.Right, targetSum-root.Val)
+	}
 	return false
 }
