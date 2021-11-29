ae442087e8c9f11d1b4f8e20852ae819edee7370
ranzishuo <ranzishuo@bytedance.com>
2021-11-29 14:04:31 +0800

    Update

--- 100-150/113.path_sum/main.go
+++ 100-150/113.path_sum/main.go
@@ -6,7 +6,22 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+// 借用112
+var res [][]int
 func pathSum(root *TreeNode, targetSum int) [][]int {
+	res = make([][]int, 0)
 
-	return nil
+	recursion(root, targetSum, make([]int, 0))
+	return res
+}
+
+func recursion(root *TreeNode, targetSum int, one []int) {
+	if root == nil { return }
+	if targetSum == root.Val && root.Left == nil && root.Right == nil {
+		one = append(one, root.Val)
+		res = append(res, append(make([]int, 0), one...))
+		return
+	}
+	recursion(root.Left, targetSum-root.Val, append(one, root.Val))
+	recursion(root.Right, targetSum-root.Val, append(one, root.Val))
 }
