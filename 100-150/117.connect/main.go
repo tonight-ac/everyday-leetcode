94c235693c1c7cc21c905167e808d5bd092abdd5
ranzishuo <ranzishuo@bytedance.com>
2021-11-29 17:19:31 +0800

    Update

--- 100-150/117.connect/main.go
+++ 100-150/117.connect/main.go
@@ -7,7 +7,26 @@ type Node struct {
 	Next *Node
 }
 
+// 参考116
 func connect(root *Node) *Node {
+	if root == nil { return nil }
 
-	return nil
+	connectTwo(root.Left, root.Right)
+
+	return root
+}
+
+func connectTwo(one, two *Node) {
+	if one != nil {
+		one.Next = two
+		connectTwo(one.Left, one.Right)
+	}
+
+	if two != nil {
+		connectTwo(two.Left, two.Right)
+	}
+
+	if one != nil && two != nil {
+		connectTwo(one.Right, two.Left)
+	}
 }
