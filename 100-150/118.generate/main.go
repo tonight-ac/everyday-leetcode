6808860a1d66441249b68386e7f92b132f35cf08
ranzishuo <ranzishuo@bytedance.com>
2021-11-29 17:27:24 +0800

    Update

--- 100-150/118.generate/main.go
+++ 100-150/118.generate/main.go
@@ -1,6 +1,21 @@
 package main
 
 func generate(numRows int) [][]int {
+	res := make([][]int, numRows)
 
-	return nil
+	res[0] = []int{1}
+
+	for i := 2; i <= numRows; i++ {
+		res[i] = make([]int, i)
+		for j := 0; j < len(res[i]); j++ {
+			if j > 0 {
+				res[i][j] += res[i-1][j-1]
+			}
+			if j < len(res[i-1]) {
+				res[i][j] += res[i-1][j]
+			}
+		}
+	}
+
+	return res
 }
