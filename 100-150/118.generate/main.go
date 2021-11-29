57640d197dcaf88cbc24ad0eacd35cae38412363
ranzishuo <ranzishuo@bytedance.com>
2021-11-29 17:49:20 +0800

    Update

--- 100-150/118.generate/main.go
+++ 100-150/118.generate/main.go
@@ -1,12 +1,18 @@
 package main
 
+import "fmt"
+
+func main() {
+	fmt.Println(generate(6))
+}
+
 func generate(numRows int) [][]int {
 	res := make([][]int, numRows)
 
 	res[0] = []int{1}
 
 	for i := 1; i < numRows; i++ {
-		res[i] = make([]int, i)
+		res[i] = make([]int, i+1)
 		for j := 0; j < len(res[i]); j++ {
 			if j > 0 {
 				res[i][j] += res[i-1][j-1]
--- 100-150/119.get_row/main.go
+++ 100-150/119.get_row/main.go
@@ -2,7 +2,31 @@ package main
 
 // 做多33行
 var res [34]int
+func init() {
+	res[0] = 1
+}
+//[
+//[1] 0
+//[1 1] 1
+//[1 2 1] 2     0. (i-1)+(i-1) 2*i-1-(i-2)
+//[1 3 3 1] 3   0. (i-1)+(i-2)
+//[1 4 6 4 1] 4 0. (i-1)+(i-3) (i-1)+(i-1) 2*i-1-1*1
+//[1 5 10 10 5 1] 5 i (i-1)+(i-2)+(i-2) 3*i-1-2*2
+//]
+
+// 先填前rowIndex/2+1 后面的对称复制
 func getRow(rowIndex int) []int {
+	// 先填前rowIndex/2+1
+	i := 0
+	for ; i < rowIndex/2+1; i++ {
+		// 根据i判断当前下标数值，这是一个找规律题目
+
+	}
+
+	// 对称复制
+	for ; i <= rowIndex; i++ {
+		res[i] = res[rowIndex-i] // TODO
+	}
 
 	return res[:rowIndex+1]
 }
