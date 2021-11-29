b53fbdf85a2f919a7cbca5d5ae110c342deb1361
tonight-ac <1531728675@qq.com>
2021-11-29 22:29:28 +0800

    Update

--- 100-150/120.minimum_total/main.go
+++ 100-150/120.minimum_total/main.go
@@ -1,12 +1,22 @@
 package main
 
+import "fmt"
+
+func main() {
+	fmt.Println(minimumTotal([][]int{{-1},{3,2},{1,-2,-1}}))
+}
+//[
+//[-1],
+//[3,2],
+//[1,-2,-1]
+//]
 func minimumTotal(triangle [][]int) int {
 	for i := 1; i < len(triangle); i++ {
 		for j := 0; j < len(triangle[i]); j++ {
 			if j == 0 {
-				triangle[i][j] += triangle[i-1][j-1]
-			} else if j == len(triangle[i-1]) - 1 {
 				triangle[i][j] += triangle[i-1][j]
+			} else if j == len(triangle[i]) - 1 {
+				triangle[i][j] += triangle[i-1][j-1]
 			} else {
 				triangle[i][j] += min(triangle[i-1][j-1], triangle[i-1][j])
 			}
