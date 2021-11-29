a02d7134476335171d326bec3002eef00ece7c3a
ranzishuo <ranzishuo@bytedance.com>
2021-11-29 14:32:11 +0800

    Update

--- 100-150/105.build_tree/main.go
+++ 100-150/105.build_tree/main.go
@@ -6,7 +6,22 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+//Input: preorder = [3, 9,1, 20,15,7], inorder = [1,9, 3, 15,20,7]
+//Output: [3,9,20,null,null,15,7]
+//    3
+//  9  20
+// 1  15 7
+// 递归构建
+// 分组找规律
+//preorder = [3, 9,1, 20,15,7], inorder = [1,9, 3, 15,20,7]
 func buildTree(preorder []int, inorder []int) *TreeNode {
-
-	return nil
+	if len(preorder) == 0 { return nil }
+	if len(preorder) == 1 { return &TreeNode{Val: preorder[0]} }
+	idx := 0
+	for idx < len(inorder) && inorder[idx] != preorder[0] { idx++ }
+	return &TreeNode{
+		Val: preorder[0],
+		Left: buildTree(preorder[1:idx+1], inorder[0:idx]),
+		Right: buildTree(preorder[idx+1:], inorder[idx+1:]),
+	}
 }
