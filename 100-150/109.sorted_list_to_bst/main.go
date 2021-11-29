4b7f272a6aabe621d8f2cf27e7bac15a1956ee17
ranzishuo <ranzishuo@bytedance.com>
2021-11-29 13:01:40 +0800

    Update

--- 100-150/109.sorted_list_to_bst/main.go
+++ 100-150/109.sorted_list_to_bst/main.go
@@ -16,11 +16,28 @@ type TreeNode struct {
 func sortedListToBST(head *ListNode) *TreeNode {
 	if head == nil { return nil }
 	if head.Next == nil { return &TreeNode{ Val: head.Val }}
-	slow, quick := head, head
-	// 1 2 3 4 5
+	left, mid, right := split(head)
+	return &TreeNode{
+		Val: mid.Val,
+		Left: sortedListToBST(left),
+		Right: sortedListToBST(right),
+	}
+}
+
+func split(head *ListNode) (left *ListNode, mid *ListNode, right *ListNode){
+	// 两个不好处理
+	if head.Next.Next == nil { return nil, head, head.Next }
+
+	prev, slow, quick := head, head, head.Next
+
 	for quick != nil && quick.Next != nil {
+		prev = slow
 		slow = slow.Next
 		quick = quick.Next.Next
 	}
-	return nil
+
+	// 对于left形成尾部
+	prev.Next = nil
+
+	return head, slow, slow.Next
 }
