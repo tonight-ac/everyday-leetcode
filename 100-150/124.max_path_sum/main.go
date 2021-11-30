3332f0c8f513b35a3f29632d0734ae0a78dc956c
ranzishuo <ranzishuo@bytedance.com>
2021-11-30 15:00:21 +0800

    Update

--- 100-150/124.max_path_sum/main.go
+++ 100-150/124.max_path_sum/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "sort"
+
 type TreeNode struct {
 	Val int
 	Left *TreeNode
@@ -7,6 +9,19 @@ type TreeNode struct {
 }
 
 func maxPathSum(root *TreeNode) int {
+	// 三个起码选一个，最多选三个，要求和最大
+	nums := []int{ root.Val }
+	if root.Left != nil { nums = append(nums, maxPathSum(root.Left)) }
+	if root.Right != nil { nums = append(nums, maxPathSum(root.Right)) }
+	sort.Ints(nums)
+	if nums[len(nums)-1] <= 0 {
+		return nums[len(nums)-1]
+	}
 
-	return 0
+	tot := 0
+	for i := len(nums)-1; i >= 0 && nums[i] >= 0; i-- {
+		tot += nums[i]
+	}
+
+	return tot
 }
