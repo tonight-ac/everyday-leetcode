26ddffd10f1a8c9bb7830de3b10b99f53787c04b
ranzishuo <ranzishuo@bytedance.com>
2021-11-30 14:06:14 +0800

    Update

--- 100-150/122.max_profit/main.go
+++ 100-150/122.max_profit/main.go
@@ -2,6 +2,11 @@ package main
 
 // 寻找升序子序列
 func maxProfit(prices []int) int {
-
+	res := 0
+	for i := 1; i < len(prices); i++ {
+		if prices[i] > prices[i-1] {
+			res += prices[i]-prices[i-1]
+		}
+	}
 	return 0
 }
