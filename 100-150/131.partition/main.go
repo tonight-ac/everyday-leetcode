dad4c580cbca461804cdbd084a98f2489a8e3406
ranzishuo <ranzishuo@bytedance.com>
2021-11-30 17:21:37 +0800

    Update

--- 100-150/131.partition/main.go
+++ 100-150/131.partition/main.go
@@ -4,12 +4,20 @@ package main
 // #b#a#a#
 // a a b aa
 
-//   a a b
-// b
-// a
-// a
-func partition(s string) [][]string {
-	// 把字符串反转，搞一个dp
 
+// 把字符串反转，搞一个dp
+//
+func partition(s string) [][]string {
+	//dp[i][j] 表示i到j之间的所有子串组合
+	for i := 0; i < len(s); i++ {
+		for j := i; j < len(s); j++ {
+
+		}
+	}
 	return nil
+}
+
+func isPalindrome(s string) bool {
+
+	return true
 }
