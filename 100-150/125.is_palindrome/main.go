e1ca60ed5ab8e3904f0831f581ae4eefecf43b1a
ranzishuo <ranzishuo@bytedance.com>
2021-11-30 15:41:35 +0800

    Update

--- 100-150/125.is_palindrome/main.go
+++ 100-150/125.is_palindrome/main.go
@@ -1,6 +1,29 @@
 package main
 
-func isPalindrome(s string) bool {
+import (
+	"strings"
+	"unicode"
+)
 
-	return false
+func isPalindrome(s string) bool {
+	// 清洗s
+	var b strings.Builder
+	for i := 0; i < len(s); i++ {
+		r := rune(s[i])
+		if unicode.IsDigit(r) || unicode.IsLower(r) {
+			b.WriteRune(r)
+		} else if unicode.IsUpper(r) {
+			b.WriteRune(r)
+		}
+	}
+
+	// 判断是否回文
+	ss := b.String()
+	for i, j := 0, len(ss)-1; i < j; i, j = i+1, j-1 {
+		if ss[i] != ss[j] {
+			return false
+		}
+	}
+
+	return true
 }
