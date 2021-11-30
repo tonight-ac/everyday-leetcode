6175ccdde6e2f74249139717399636832ea4ef93
tonight-ac <1531728675@qq.com>
2021-11-30 22:47:57 +0800

    Update

--- 100-150/121.max_profit/main.go
+++ 100-150/121.max_profit/main.go
@@ -1,13 +1,17 @@
 package main
 
+import "fmt"
+
+func main() {
+	fmt.Println(maxProfit([]int{7,1,5,3,6,4}))
+}
+
 func maxProfit(prices []int) int {
 	res := 0
 	min := prices[0]
 	for i := 1; i < len(prices); i++ {
-		if prices[i] > min {
-			if res < prices[i] - min {
-				res = prices[i] - min
-			}
+		if prices[i] > min && res < prices[i] - min {
+			res = prices[i] - min
 		} else {
 			min = prices[i]
 		}
--- 100-150/122.max_profit/main.go
+++ 100-150/122.max_profit/main.go
@@ -1,5 +1,11 @@
 package main
 
+import "fmt"
+
+func main() {
+	fmt.Println(maxProfit([]int{7,1,5,3,6,4}))
+}
+
 // 寻找升序子序列
 func maxProfit(prices []int) int {
 	res := 0
@@ -8,5 +14,5 @@ func maxProfit(prices []int) int {
 			res += prices[i]-prices[i-1]
 		}
 	}
-	return 0
+	return res
 }
