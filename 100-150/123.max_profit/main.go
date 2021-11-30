eb2ea5220711c8351c83704e86b9b8539ff255fb
tonight-ac <1531728675@qq.com>
2021-11-30 22:55:27 +0800

    Update

--- 100-150/123.max_profit/main.go
+++ 100-150/123.max_profit/main.go
@@ -3,31 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(maxProfit([]int{3,3,5,0,0,3,1,4}))
+	fmt.Println(maxProfit([]int{1,2,3,4,5}))
 }
 
+// 超时了nmd
 func maxProfit(prices []int) int {
-	dp := make([][]int, len(prices))
-	for i := 0; i < len(dp); i++ { dp[i] = make([]int, len(prices)) }
-
-	for i := 0; i < len(prices); i++ {
-		for j := i+1; j < len(prices); j++ {
-			dp[i][j] = oneMaxProfit(prices[i:j+1])
-		}
-	}
-
-	// 分为左右两份
 	res := 0
 	for i := 0; i < len(prices); i++ {
-		if res < dp[0][i] + dp[i][len(prices)-1] {
-			res = dp[0][i] + dp[i][len(prices)-1]
+		left := oneMaxProfit(prices[:i])
+		right := oneMaxProfit(prices[i:])
+		if res < left + right {
+			res = left + right
 		}
 	}
-
 	return res
 }
 
 func oneMaxProfit(prices []int) int {
+	if len(prices) == 0 { return 0 }
 	res := 0
 	min := prices[0]
 	for i := 1; i < len(prices); i++ {
@@ -41,6 +34,28 @@ func oneMaxProfit(prices []int) int {
 	}
 	return res
 }
+
+// 空间超了，搞一份不需要存储的
+//func maxProfit(prices []int) int {
+	//dp := make([][]int, len(prices))
+	//for i := 0; i < len(dp); i++ { dp[i] = make([]int, len(prices)) }
+
+	//for i := 0; i < len(prices); i++ {
+	//	for j := i+1; j < len(prices); j++ {
+	//		dp[i][j] = oneMaxProfit(prices[i:j+1])
+	//	}
+	//}
+	//
+	//分为左右两份
+	//res := 0
+	//for i := 0; i < len(prices); i++ {
+	//	if res < dp[0][i] + dp[i][len(prices)-1] {
+	//		res = dp[0][i] + dp[i][len(prices)-1]
+	//	}
+	//}
+
+	//return res
+//}
 
 // 寻找能赚钱的交易，选最大两次
 //func maxProfit(prices []int) int {
