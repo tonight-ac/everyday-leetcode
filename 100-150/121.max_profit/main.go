ff89326e159884aa3b264de51bedbb61345a7937
ranzishuo <ranzishuo@bytedance.com>
2021-12-01 11:06:41 +0800

    Update

--- 100-150/121.max_profit/main.go
+++ 100-150/121.max_profit/main.go
@@ -10,8 +10,10 @@ func maxProfit(prices []int) int {
 	res := 0
 	min := prices[0]
 	for i := 1; i < len(prices); i++ {
-		if prices[i] > min && res < prices[i] - min {
-			res = prices[i] - min
+		if prices[i] > min {
+			if  res < prices[i] - min {
+				res = prices[i] - min
+			}
 		} else {
 			min = prices[i]
 		}
--- 100-150/123.max_profit/main.go
+++ 100-150/123.max_profit/main.go
@@ -8,27 +8,18 @@ func main() {
 
 // 寻找能赚钱的交易，选最大两次
 func maxProfit(prices []int) int {
-	start := 0
-	wave := make([][]int, 0)
-
-	for i := 1; i < len(prices); i++ {
-		if prices[i] <= prices[i-1] {
-			//if prices[i] != prices[i-1] {
-				wave = append(wave, []int{start, i-1})
-			//}
-			start = i
-		}
-	}
-
 	res := 0
-	// 可操作波段数量小于2
-	if len(wave) <= 2 {
-		for i := 0; i < len(wave); i++ {
-			res += prices[wave[i][1]] - prices[wave[i][0]]
+	min := prices[0]
+	for i := 1; i < len(prices); i++ {
+		if prices[i] > min {
+			if  res < prices[i] - min {
+				res = prices[i] - min
+			}
+		} else {
+			min = prices[i]
 		}
 	}
 
-	// 需要对wave搜索
 	return 0
 }
 
