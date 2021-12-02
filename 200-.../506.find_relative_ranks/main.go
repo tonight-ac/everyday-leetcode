3b3046a1a0f520e7740db43910b988360d61b4c8
tonight-ac <1531728675@qq.com>
2021-12-03 00:09:46 +0800

    Uodate

--- 200-.../506.find_relative_ranks/main.go
+++ 200-.../506.find_relative_ranks/main.go
@@ -1,7 +1,25 @@
 package main
 
-// TODO未完成
-func findRelativeRanks(score []int) []string {
+import "sort"
 
-	return nil
+var desc = [3]string{"Gold Medal", "Silver Medal", "Bronze Medal"}
+
+func findRelativeRanks(score []int) []string {
+	n := len(score)
+	type pair struct{ score, idx int }
+	arr := make([]pair, n)
+	for i, s := range score {
+		arr[i] = pair{s, i}
+	}
+	sort.Slice(arr, func(i, j int) bool { return arr[i].score > arr[j].score })
+
+	ans := make([]string, n)
+	for i, p := range arr {
+		if i < 3 {
+			ans[p.idx] = desc[i]
+		} else {
+			ans[p.idx] = strconv.Itoa(i + 1)
+		}
+	}
+	return ans
 }
