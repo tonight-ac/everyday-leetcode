1285ae3b11cb965ce0ea53879d330e9328e9db32
ranzishuo <ranzishuo@bytedance.com>
2021-12-03 16:57:05 +0800

    Updat

--- 100-150/137.single_number/main.go
+++ 100-150/137.single_number/main.go
@@ -1,6 +1,20 @@
 package main
 
 func singleNumber(nums []int) int {
+	bits := make([]uint32, 0)
+	for i := 0; i < len(nums); i++ {
+		n := uint32(nums[i])
+		for j := 0; n != 0; j++ {
+			bits[j] += n % 2
+			n >>= 1
+		}
+	}
 
-	return 0
+	res := uint32(0)
+	for i := 0; i < len(bits); i++ {
+		res += bits[i] % 3
+		res <<= 1
+	}
+
+	return int(res)
 }
