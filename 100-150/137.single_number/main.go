aac11551f660f4cf65d9f343497f031996196e29
tonight-ac <1531728675@qq.com>
2021-12-04 17:01:27 +0800

    Update

--- 100-150/137.single_number/main.go
+++ 100-150/137.single_number/main.go
@@ -1,20 +1,36 @@
 package main
 
+import "fmt"
+
+func main() {
+	n := -1
+	fmt.Println(n<<1)
+	//fmt.Println(singleNumber([]int{-1,-1,-2,-1}))
+}
+
 func singleNumber(nums []int) int {
-	bits := make([]uint32, 0)
+	bits := make([]int32, 32)
+
+	for i := 0; i < 32; i++ {
+		bits[i] = ^(1 << i)
+	}
+
 	for i := 0; i < len(nums); i++ {
-		n := uint32(nums[i])
-		for j := 0; n != 0; j++ {
-			bits[j] += n % 2
-			n >>= 1
+		for j := 0; j < len(bits); j++ {
+			n := int32(nums[i])
+			n &= bits[j]
+			bits[j] ^= n
 		}
 	}
 
-	res := uint32(0)
-	for i := 0; i < len(bits); i++ {
-		res += bits[i] % 3
-		res <<= 1
-	}
+	//res := 0
+	//for i := 0; i < len(bits); i++ {
+	//	res += (bits[i] % 3) << i
+	//}
 
-	return int(res)
+	//if bits[len(bits)-1] % 3 == 0 {
+	//	return int(res)
+	//}
+	//return -int(res)
+	return 0
 }
