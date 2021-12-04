ac24e803915537b5b505a80ee26bd84620847498
tonight-ac <1531728675@qq.com>
2021-12-04 20:45:08 +0800

    Update

--- 100-150/144.preorder_traversal/main.go
+++ 100-150/144.preorder_traversal/main.go
@@ -18,13 +18,52 @@ func main() {
 			},
 		},
 	}))
+	//fmt.Println(preorderTraversal(&TreeNode{
+	//	Val: 2,
+	//	Left: &TreeNode{
+	//		Val: 1,
+	//		Right: &TreeNode{
+	//			Val: 4,
+	//		},
+	//	},
+	//	Right: &TreeNode{
+	//		Val: 3,
+	//	},
+	//}))
+}
+
+// 迭代算法
+func preorderTraversal(root *TreeNode) []int {
+	if root == nil { return nil }
+	left := append(make([]*TreeNode, 0), root)
+	right := make([]*TreeNode, 0)
+	res := make([]int, 0)
+
+	for {
+		var first *TreeNode
+		if len(left) != 0 {
+			first = left[0]
+			left = left[1:]
+		} else if len(right) != 0 {
+			first = right[0]
+			right = right[1:]
+		} else {
+			break
+		}
+
+		res = append(res, first.Val)
+		if first.Left != nil { left = append(left, first.Left) }
+		if first.Right != nil { right = append(append(make([]*TreeNode, 0), first.Right), right...) }
+	}
+
+	return res
 }
 
 // 递归算法
 // 消耗内存但优美版
-func preorderTraversal(root *TreeNode) []int {
-	if root == nil { return nil }
-	left := preorderTraversal(root.Left)
-	right := preorderTraversal(root.Right)
-	return append(append(append(make([]int, 0), root.Val), left...), right...)
-}
+//func preorderTraversal(root *TreeNode) []int {
+//	if root == nil { return nil }
+//	left := preorderTraversal(root.Left)
+//	right := preorderTraversal(root.Right)
+//	return append(append(append(make([]int, 0), root.Val), left...), right...)
+//}
