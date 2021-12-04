94ed207644babea8aece79f56420a06cf5360fa2
tonight-ac <1531728675@qq.com>
2021-12-04 21:24:14 +0800

    Update

--- 100-150/144.preorder_traversal/main.go
+++ 100-150/144.preorder_traversal/main.go
@@ -35,25 +35,25 @@ func main() {
 // 迭代算法
 func preorderTraversal(root *TreeNode) []int {
 	if root == nil { return nil }
-	left := append(make([]*TreeNode, 0), root)
+	left := make([]*TreeNode, 0)
 	right := make([]*TreeNode, 0)
 	res := make([]int, 0)
 
 	for {
-		var first *TreeNode
 		if len(left) != 0 {
-			first = left[0]
+			root = left[0]
 			left = left[1:]
 		} else if len(right) != 0 {
-			first = right[0]
+			root = right[0]
 			right = right[1:]
-		} else {
+		} else if root == nil {
 			break
 		}
 
-		res = append(res, first.Val)
-		if first.Left != nil { left = append(left, first.Left) }
-		if first.Right != nil { right = append(append(make([]*TreeNode, 0), first.Right), right...) }
+		res = append(res, root.Val)
+		if root.Left != nil { left = append(left, root.Left) }
+		if root.Right != nil { right = append(append(make([]*TreeNode, 0), root.Right), right...) }
+		root = nil
 	}
 
 	return res
--- 100-150/145.postorder_traversal/main.go
+++ 100-150/145.postorder_traversal/main.go
@@ -1,16 +1,67 @@
 package main
 
+import "fmt"
+
 type TreeNode struct {
 	Val int
 	Left *TreeNode
 	Right *TreeNode
 }
 
-// 递归算法
-// 消耗内存但优美版
+func main() {
+	fmt.Println(postorderTraversal(&TreeNode{
+		Val: 1,
+		Right: &TreeNode{
+			Val: 2,
+			Left: &TreeNode{
+				Val: 3,
+			},
+		},
+	}))
+}
+
+// 1
+//  3
+// 2
+
+// 迭代算法
 func postorderTraversal(root *TreeNode) []int {
 	if root == nil { return nil }
-	left := postorderTraversal(root.Left)
-	right := postorderTraversal(root.Right)
-	return append(left, append(right, root.Val)...)
+
+	in := append(make([]*TreeNode, 0), root)
+
+	res := make([]int, 0)
+
+	for {
+		var first *TreeNode
+		if len(in) != 0 {
+			first = in[len(in)-1]
+		} else {
+			break
+		}
+
+		if first.Left == nil && first.Right == nil {
+			in = in[:len(in)-1]
+			res = append(res, first.Val)
+		}
+		if first.Right != nil {
+			in = append(in, first.Right)
+			first.Right = nil
+		}
+		if first.Left != nil {
+			in = append(in, first.Left)
+			first.Left = nil
+		}
+	}
+
+	return res
 }
+
+// 递归算法
+// 消耗内存但优美版
+//func postorderTraversal(root *TreeNode) []int {
+//	if root == nil { return nil }
+//	left := postorderTraversal(root.Left)
+//	right := postorderTraversal(root.Right)
+//	return append(left, append(right, root.Val)...)
+//}
