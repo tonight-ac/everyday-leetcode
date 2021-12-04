3fd73089e99be881be8eb0d55f8641ccd816e544
tonight-ac <1531728675@qq.com>
2021-12-04 20:23:39 +0800

    Update

--- 100-150/145.postorder_traversal/main.go
+++ 100-150/145.postorder_traversal/main.go
@@ -6,7 +6,11 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+// 递归算法
+// 消耗内存但优美版
 func postorderTraversal(root *TreeNode) []int {
-
-	return nil
+	if root == nil { return nil }
+	left := postorderTraversal(root.Left)
+	right := postorderTraversal(root.Right)
+	return append(left, append(right, root.Val)...)
 }
