77e3217944569c20f726d1f385403f7eb1855d78
tonight-ac <1531728675@qq.com>
2021-12-04 17:48:21 +0800

    Update

--- 100-150/141.has_cycle/main.go
+++ 100-150/141.has_cycle/main.go
@@ -6,6 +6,14 @@ type ListNode struct {
 }
 
 func hasCycle(head *ListNode) bool {
+	if head == nil || head.Next == nil { return false }
+	slow, fast := head, head.Next
+
+	for fast != nil && fast.Next != nil {
+		if slow == fast { return true }
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
 
 	return false
 }
