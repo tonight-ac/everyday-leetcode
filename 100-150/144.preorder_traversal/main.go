255da801d5180003c0c38c8ce045bdc5b78e13d8
tonight-ac <1531728675@qq.com>
2021-12-04 20:22:06 +0800

    Update

--- 100-150/144.preorder_traversal/main.go
+++ 100-150/144.preorder_traversal/main.go
@@ -1,12 +1,30 @@
 package main
 
+import "fmt"
+
 type TreeNode struct {
 	Val int
 	Left *TreeNode
 	Right *TreeNode
 }
 
-func preorderTraversal(root *TreeNode) []int {
+func main() {
+	fmt.Println(preorderTraversal(&TreeNode{
+		Val: 1,
+		Right: &TreeNode{
+			Val: 2,
+			Left: &TreeNode{
+				Val: 3,
+			},
+		},
+	}))
+}
 
-	return nil
+// 递归算法
+// 消耗内存但优美版
+func preorderTraversal(root *TreeNode) []int {
+	if root == nil { return nil }
+	left := preorderTraversal(root.Left)
+	right := preorderTraversal(root.Right)
+	return append(append(append(make([]int, 0), root.Val), left...), right...)
 }
