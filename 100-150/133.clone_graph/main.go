f9dbc1b6d550790600f677d87ff51699c9e9abe6
tonight-ac <1531728675@qq.com>
2021-12-06 00:13:09 +0800

    Update

--- 100-150/133.clone_graph/main.go
+++ 100-150/133.clone_graph/main.go
@@ -8,6 +8,7 @@ type Node struct {
 var uni map[*Node]bool
 func cloneGraph(node *Node) *Node {
 	uni = make(map[*Node]bool)
+	return nil
 }
 
 func recursion(node *Node) *Node {
@@ -23,11 +24,11 @@ func recursion(node *Node) *Node {
 
 // 内存溢出了，这是无向 连通 图
 //func cloneGraph(node *Node) *Node {
-	if node == nil { return nil }
-	if len(node.Neighbors) == 0 { return &Node{ Val: node.Val, Neighbors: node.Neighbors }}
-	n := &Node{ Val: node.Val, Neighbors: make([]*Node, 0) }
-	for _, v := range node.Neighbors {
-		n.Neighbors = append(n.Neighbors, cloneGraph(v))
-	}
-	return n
+//	if node == nil { return nil }
+//	if len(node.Neighbors) == 0 { return &Node{ Val: node.Val, Neighbors: node.Neighbors }}
+//	n := &Node{ Val: node.Val, Neighbors: make([]*Node, 0) }
+//	for _, v := range node.Neighbors {
+//		n.Neighbors = append(n.Neighbors, cloneGraph(v))
+//	}
+//	return n
 //}
---
+++ 200-.../1816.truncate_sentence/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "strings"
+
+func truncateSentence(s string, k int) string {
+	return strings.Join(strings.Split(s, " ")[:k], " ")
+}
