df8e0c976a664904d7041c5478e503c66cdf6044
ranzishuo <ranzishuo@bytedance.com>
2021-12-05 13:04:19 +0800

    Update

--- 100-150/131.partition/main.go
+++ 100-150/131.partition/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 // #a#a#b#
@@ -13,8 +12,8 @@ import (
 
 
 func main() {
-	fmt.Println(findAllSubPalindrome("efe"))
-	//fmt.Println(partition("aab"))
+	//fmt.Println(countSubstrings("efe"))
+	fmt.Println(partition("efe"))
 }
 
 // 先找到所有回文子串的下标，然后搞搜索
@@ -23,7 +22,7 @@ var res [][]string
 var ss string
 func partition(s string) [][]string {
 	ss = s
-	m = findAllSubPalindrome(s)
+	m = countSubstrings(s)
 	res = make([][]string, 0)
 
 	recursion(0, make([]string, 0))
@@ -43,39 +42,25 @@ func recursion(i int, list []string) {
 	}
 }
 
-// 找到所有回文子串的下标 TODO
+// 找到所有回文子串的下标
 // 需要参考647题
-func findAllSubPalindrome(s string) map[int]map[int]interface{} {
-	res := make(map[int]map[int]interface{})
+func countSubstrings(s string) map[int]map[int]interface{} {
+	mm := make(map[int]map[int]interface{})
+	for i := 0; i < len(s); i++ { mm[i] = make(map[int]interface{}) }
 
-	ss := "#" + strings.Join(strings.Split(s, ""), "#") + "#"
-
-	i, j := 0, 0
-
-	for i = 0; i < len(ss); i++ {
-		for j = i + 1; j < len(ss); j++ {
-			if 2 * i - j < 0 || ss[j] != ss[2 * i - j] {
-				break
-			}
-		}
-
-		realI, realJ := (2*i-(j-1))/2, (j-1)/2
-		if res[realI] == nil {
-			res[realI] = make(map[int]interface{})
-		}
-		if realI != realJ {
-			res[realI][realJ] = nil
+	// 单中心
+	for i := 0; i < len(s); i++ {
+		for l, r := i, i; l >= 0 && r < len(s) && s[l] == s[r]; l, r = l-1, r+1 {
+			mm[l][r+1] = nil
 		}
 	}
 
-	return res
+	// 双中心
+	for i := 1; i < len(s); i++ {
+		for l, r := i-1, i; l >= 0 && r < len(s) && s[l] == s[r]; l, r = l-1, r+1 {
+			mm[l][r+1] = nil
+		}
+	}
+
+	return mm
 }
-
-
-//func isPalindrome(s string) bool {
-//	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
-//		if s[i] != s[j] { return false }
-//	}
-//
-//	return true
-//}
