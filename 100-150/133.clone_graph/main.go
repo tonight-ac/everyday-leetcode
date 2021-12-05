cc30724ed68059f9b2bee3c2a3540f310b0c9a44
tonight-ac <1531728675@qq.com>
2021-12-05 21:15:04 +0800

    Update

--- 100-150/133.clone_graph/main.go
+++ 100-150/133.clone_graph/main.go
@@ -5,8 +5,14 @@ type Node struct {
 	Neighbors []*Node
 }
 
+var uni map[*Node]bool
 func cloneGraph(node *Node) *Node {
+	uni = make(map[*Node]bool)
+}
+
+func recursion(node *Node) *Node {
 	if node == nil { return nil }
+	if uni[node] { return nil }
 	if len(node.Neighbors) == 0 { return &Node{ Val: node.Val, Neighbors: node.Neighbors }}
 	n := &Node{ Val: node.Val, Neighbors: make([]*Node, 0) }
 	for _, v := range node.Neighbors {
@@ -14,3 +20,14 @@ func cloneGraph(node *Node) *Node {
 	}
 	return n
 }
+
+// 内存溢出了，这是无向 连通 图
+//func cloneGraph(node *Node) *Node {
+	if node == nil { return nil }
+	if len(node.Neighbors) == 0 { return &Node{ Val: node.Val, Neighbors: node.Neighbors }}
+	n := &Node{ Val: node.Val, Neighbors: make([]*Node, 0) }
+	for _, v := range node.Neighbors {
+		n.Neighbors = append(n.Neighbors, cloneGraph(v))
+	}
+	return n
+//}
