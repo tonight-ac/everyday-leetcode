963269ab28d05a52fefcb70df9629d6ba9cccd59
ranzishuo <ranzishuo@bytedance.com>
2021-12-05 13:14:46 +0800

    Update

--- 001-050/05.longest_palindrome/main.go
+++ 001-050/05.longest_palindrome/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 func main() {
@@ -10,25 +9,52 @@ func main() {
 }
 
 func longestPalindrome(s string) string {
-	maxI, maxJ := 0, 0
+	if len(s) == 0 { return "" }
 
-	ss := "#" + strings.Join(strings.Split(s, ""), "#") + "#"
+	maxR, maxL := 0, 0
 
-	i, j := 0, 0
-
-	for i = 0; i < len(ss); i++ {
-		for j = i + 1; j < len(ss); j++ {
-			if 2 * i - j < 0 || ss[j] != ss[2 * i - j] {
-				break
-			}
+	// 单中心
+	for i := 0; i < len(s); i++ {
+		l, r := i, i
+		for ; l >= 0 && r < len(s); l, r = l-1, r+1 {
+			if s[l] != s[r] { break }
 		}
-		// 需要移动中心，先记下最大值
-		if j - i > maxJ - maxI {
-			maxJ, maxI = j, i
-		}
+		if maxR - maxL < r - l { maxR, maxL = r, l }
 	}
 
-	// 2*maxI-(maxJ-1) 左边界
-	// 2:(maxJ-1) 右边界
-	return s[(2*maxI-(maxJ-1))/2:(maxJ-1)/2]
+	// 双中心
+	for i := 1; i < len(s); i++ {
+		l, r := i-1, i
+		if s[l] != s[r] { continue }
+		for ; l >= 0 && r < len(s); l, r = l-1, r+1 {
+			if s[l] != s[r] { break }
+		}
+		if maxR - maxL < r - l { maxR, maxL = r, l }
+	}
+
+	return s[maxL+1:maxR]
 }
+
+//func longestPalindrome(s string) string {
+//	maxI, maxJ := 0, 0
+//
+//	ss := "#" + strings.Join(strings.Split(s, ""), "#") + "#"
+//
+//	i, j := 0, 0
+//
+//	for i = 0; i < len(ss); i++ {
+//		for j = i + 1; j < len(ss); j++ {
+//			if 2 * i - j < 0 || ss[j] != ss[2 * i - j] {
+//				break
+//			}
+//		}
+//		// 需要移动中心，先记下最大值
+//		if j - i > maxJ - maxI {
+//			maxJ, maxI = j, i
+//		}
+//	}
+//
+//	// 2*maxI-(maxJ-1) 左边界
+//	// 2:(maxJ-1) 右边界
+//	return s[(2*maxI-(maxJ-1))/2:(maxJ-1)/2]
+//}
