6c5b6568ce8a7ca44c376477ffac1ce1851b56ad
ranzishuo <ranzishuo@bytedance.com>
2021-12-05 13:00:35 +0800

    Update

--- 200-.../647.count_substrings/main.go
+++ 200-.../647.count_substrings/main.go
@@ -1,6 +1,26 @@
 package main
 
-func countSubstrings(s string) int {
+import "fmt"
 
-	return 0
+func main() {
+	fmt.Println(countSubstrings("abc"))
+}
+func countSubstrings(s string) int {
+	res := 0
+
+	// 单中心
+	for i := 0; i < len(s); i++ {
+		for l, r := i, i; l >= 0 && r < len(s) && s[l] == s[r]; l, r = l-1, r+1 {
+			res++
+		}
+	}
+
+	// 双中心
+	for i := 1; i < len(s); i++ {
+		for l, r := i-1, i; l >= 0 && r < len(s) && s[l] == s[r]; l, r = l-1, r+1 {
+			res++
+		}
+	}
+
+	return res
 }
