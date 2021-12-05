3e8a6e0ee39c0699668b358bd58d74e9813be007
ranzishuo <ranzishuo@bytedance.com>
2021-12-05 14:26:26 +0800

    Update

--- 100-150/133.clone_graph/main.go
+++ 100-150/133.clone_graph/main.go
@@ -5,8 +5,12 @@ type Node struct {
 	Neighbors []*Node
 }
 
-
 func cloneGraph(node *Node) *Node {
-
-	return nil
+	if node == nil { return nil }
+	if len(node.Neighbors) == 0 { return &Node{ Val: node.Val, Neighbors: node.Neighbors }}
+	n := &Node{ Val: node.Val, Neighbors: make([]*Node, 0) }
+	for _, v := range node.Neighbors {
+		n.Neighbors = append(n.Neighbors, cloneGraph(v))
+	}
+	return n
 }
