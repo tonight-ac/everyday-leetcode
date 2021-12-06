43c8574b5fb93fb9c9212336714017d801cb4344
tonight-ac <1531728675@qq.com>
2021-12-06 22:56:18 +0800

    Update

--- 100-150/134.can_complete_circuit/main.go
+++ 100-150/134.can_complete_circuit/main.go
@@ -21,20 +21,47 @@ func main() {
 	fmt.Println(canCompleteCircuit([]int{5,8,2,8}, []int{6,5,6,6}))
 }
 
-// -1 -11 -1 -1 13 -1 1
-// 如果数值能够刚好持平或者更大，则要从首个最大正数的出发
+// 先搞一版暴力
+// 超时了nmd
 func canCompleteCircuit(gas []int, cost []int) int {
-	tot := 0
-	max, idx := 0, 0
 	for i := 0; i < len(gas); i++ {
 		gas[i] -= cost[i]
-		tot += gas[i]
-		if max < gas[i] {
-			max = gas[i]
-			idx = i
+	}
+
+	for i := 0; i < len(gas); i++ {
+		tot := gas[i]
+		if tot < 0 { continue }
+		for j, k := (i + 1)%len(gas), 0; k < len(gas) - 1; k, j = k + 1, (j + 1)%len(gas) {
+			tot += gas[j]
+			if tot < 0 {
+				break
+			}
+		}
+		if tot >= 0 {
+			return i
 		}
 	}
 
-	if tot < 0 { return -1 }
-	return idx
+	return -1
 }
+
+// -1 -11 -1 -1 13 -1 1
+// 如果数值能够刚好持平或者更大，则要从首个最大正数的出发
+//func canCompleteCircuit(gas []int, cost []int) int {
+	//tot := 0
+	//max, idx := 0, 0
+	//for i := 0; i < len(gas); i++ {
+	//	gas[i] -= cost[i]
+		//tot += gas[i]
+		//if max < gas[i] {
+		//	max = gas[i]
+		//	idx = i
+		//}
+	//}
+
+	//
+
+
+	//if tot < 0 { return -1 }
+	//return idx
+//}
