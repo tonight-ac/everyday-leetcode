ba54d1d1325a4af57fac98b348af7214781da6f1
tonight-ac <1531728675@qq.com>
2021-12-06 22:33:59 +0800

    Updae

--- 100-150/133.clone_graph/main.go
+++ 100-150/133.clone_graph/main.go
@@ -1,23 +1,36 @@
 package main
 
+import "fmt"
+
 type Node struct {
 	Val int
 	Neighbors []*Node
 }
 
-var uni map[*Node]bool
+func main() {
+	n1 := &Node{Val: 1, Neighbors: make([]*Node, 0)}
+	n2 := &Node{Val: 2, Neighbors: make([]*Node, 0)}
+	n3 := &Node{Val: 3, Neighbors: make([]*Node, 0)}
+	n4 := &Node{Val: 4, Neighbors: make([]*Node, 0)}
+	n1.Neighbors = append(n1.Neighbors, n2, n4)
+	n2.Neighbors = append(n2.Neighbors, n1, n3)
+	n3.Neighbors = append(n3.Neighbors, n2, n4)
+	n4.Neighbors = append(n4.Neighbors, n1, n3)
+	fmt.Println((cloneGraph(n1).Neighbors)[0])
+}
+var mapping map[*Node]*Node
 func cloneGraph(node *Node) *Node {
-	uni = make(map[*Node]bool)
-	return nil
+	mapping = make(map[*Node]*Node)
+	return recursion(node)
 }
 
 func recursion(node *Node) *Node {
 	if node == nil { return nil }
-	if uni[node] { return nil }
-	if len(node.Neighbors) == 0 { return &Node{ Val: node.Val, Neighbors: node.Neighbors }}
+	if mapping[node] != nil { return mapping[node] }
 	n := &Node{ Val: node.Val, Neighbors: make([]*Node, 0) }
+	mapping[node] = n
 	for _, v := range node.Neighbors {
-		n.Neighbors = append(n.Neighbors, cloneGraph(v))
+		n.Neighbors = append(n.Neighbors, recursion(v))
 	}
 	return n
 }
