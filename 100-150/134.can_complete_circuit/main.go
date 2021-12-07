0bd7e6ab844363c656cba0357f25afc3dc2dde12
tonight-ac <1531728675@qq.com>
2021-12-07 23:24:50 +0800

    Update

--- 100-150/134.can_complete_circuit/main.go
+++ 100-150/134.can_complete_circuit/main.go
@@ -59,20 +59,17 @@ func canCompleteCircuit(gas []int, cost []int) int {
 // -1 -11 -1 -1 13 -1 1
 // 如果数值能够刚好持平或者更大，则要从首个最大正数的出发
 //func canCompleteCircuit(gas []int, cost []int) int {
-	//tot := 0
-	//max, idx := 0, 0
-	//for i := 0; i < len(gas); i++ {
-	//	gas[i] -= cost[i]
-		//tot += gas[i]
-		//if max < gas[i] {
-		//	max = gas[i]
-		//	idx = i
-		//}
-	//}
-
-	//
-
-
-	//if tot < 0 { return -1 }
-	//return idx
+//	tot := 0
+//	max, idx := 0, 0
+//	for i := 0; i < len(gas); i++ {
+//		gas[i] -= cost[i]
+//		tot += gas[i]
+//		if max < gas[i] {
+//			max = gas[i]
+//			idx = i
+//		}
+//	}
+//
+//	if tot < 0 { return -1 }
+//	return idx
 //}
