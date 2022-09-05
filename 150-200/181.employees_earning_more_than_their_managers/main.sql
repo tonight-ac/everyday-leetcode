6e37a49b1ac96058251cbac5ab164a6d623d7c92
ranzishuo <ranzishuo@bytedance.com>
2022-09-05 16:15:05 +0800

    feat: add 181

---
+++ 150-200/181.employees_earning_more_than_their_managers/main.sql
@@ -0,0 +1,30 @@
+-- 初始化
+DROP TABLE IF EXISTS `Employee`;
+CREATE TABLE `Employee` (
+`id` INT,
+`name` VARCHAR(256),
+`salary` INT,
+`managerId` INT,
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `Employee` VALUES
+(1, 'Joe', 70000, 3),
+(2, 'Henry', 80000, 4),
+(3, 'Sam', 60000, NULL),
+(4, 'Max', 90000, NULL);
+
+-- 子查询
+SELECT `name` AS Employee FROM Employee AS t1
+WHERE managerId IS NOT NULL
+AND
+EXISTS (SELECT * FROM Employee AS t2 WHERE t1.managerId = t2.id AND t1.salary > t2.salary);
+
+-- 连接
+SELECT t1.`name` AS Employee FROM Employee AS t1
+INNER JOIN
+Employee AS t2
+ON t1.managerId = t2.id AND t1.salary > t2.salary;
+
+-- 笛卡尔积
+SElECT t1.`name` AS Employee FROM Employee AS t1, Employee AS t2
+WHERE t1.managerId = t2.id AND t1.salary > t2.salary;
