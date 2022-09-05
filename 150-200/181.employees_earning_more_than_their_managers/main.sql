050dfad7fce397e48dab05d34a670129e6bb8c46
ranzishuo <ranzishuo@bytedance.com>
2022-09-05 16:17:40 +0800

    feat: update 181

--- 150-200/181.employees_earning_more_than_their_managers/main.sql
+++ 150-200/181.employees_earning_more_than_their_managers/main.sql
@@ -13,6 +13,8 @@ INSERT INTO `Employee` VALUES
 (3, 'Sam', 60000, NULL),
 (4, 'Max', 90000, NULL);
 
+-- 添加 t1.managerId IS NOT NULL 条件可以更快
+
 -- 子查询
 SELECT `name` AS Employee FROM Employee AS t1
 WHERE managerId IS NOT NULL
@@ -23,8 +25,8 @@ EXISTS (SELECT * FROM Employee AS t2 WHERE t1.managerId = t2.id AND t1.salary > 
 SELECT t1.`name` AS Employee FROM Employee AS t1
 INNER JOIN
 Employee AS t2
-ON t1.managerId = t2.id AND t1.salary > t2.salary;
+ON t1.managerId IS NOT NULL AND t1.managerId = t2.id AND t1.salary > t2.salary;
 
 -- 笛卡尔积
 SElECT t1.`name` AS Employee FROM Employee AS t1, Employee AS t2
-WHERE t1.managerId = t2.id AND t1.salary > t2.salary;
+WHERE t1.managerId IS NOT NULL AND t1.managerId = t2.id AND t1.salary > t2.salary;
