7f0b902d930bba5073856f5d4b298c4c95119c8f
ranzishuo <ranzishuo@bytedance.com>
2022-09-11 17:16:58 +0800

    feat: update

--- 001-050/10.is_match/main.go
+++ 001-050/10.is_match/main.go
@@ -3,22 +3,31 @@ package main
 //输入：s = "mississippi" p = "mis*is*p*."
 //输出：false
 
-// 讨巧的递归做法
+// 直接相同
+// 只有 .
+// 有 * ，前面是普通字符
+// 有 * ，前面是 .
+
+// s: mississippi
+// p: mis*is*p*.
 func isMatch(s string, p string) bool {
-	// 如果p已经匹配完了，那就判断一下s有没有匹配完
-	if p == "" {
-		return s == ""
+	// s为空 p为空 为真
+	// s不为空 p不为空 不能判断
+	// s为空 p不为空 不能判断真假 ex .*
+	// s不为空 p为空 为假
+	if len(p) == 0 {
+		return len(s) == 0
 	}
 
-	// 判断第一个字符是否匹配
-	flag := s != "" && (s[0] == p[0] || p[0] == '.')
+	// 拆解为子问题，判断第一个字符
+	firstMatch := s != "" && (s[0] == p[0] || p[0] == '.')
 
 	// 判断p未来是否存在* 提前处理掉
 	if len(p) > 1 && p[1] == '*' {
 		// 跳过按空处理 或者 匹配掉向后继续匹配
-		return isMatch(s, p[2:]) || (flag && isMatch(s[1:], p))
+		return (firstMatch && isMatch(s[1:], p)) || isMatch(s, p[2:])
 	}
 
 	// 不存在通配符，直接向后处理
-	return flag && isMatch(s[1:], p[1:])
+	return firstMatch && isMatch(s[1:], p[1:])
 }
